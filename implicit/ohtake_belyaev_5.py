890f49b37b2beaab36c04c08e807c26ccea44481
sohale <sohale@gmail.com>
2016-05-18 13:11:08 +0100

    arrcos of <-1: -1.00001, separating the visualisation code.cleanup

--- implicit/ohtake_belyaev_5.py
+++ implicit/ohtake_belyaev_5.py
@@ -502,16 +502,6 @@ def delete_unused_vertices(verts, faces):
     return new_verts, new_faces
 
 
-def quick_vis(verts, facets, face_idx):
-    #print facets.shape
-    #print facets[face_idx, :].shape
-    display_simple_using_mayavi_2( [(verts, facets), (verts, facets[face_idx, :]), (verts, facets[face_idx, :])],
-       pointcloud_list=[ ], pointcloud_opacity=0.2,
-       mayavi_wireframe=[True, True, True], opacity=[0.1, 1, 0.2],
-       gradients_at=None, separate_panels=False, gradients_from_iobj=None, add_noise=[0, 0, 0.01],
-       noise_added_before_broadcast=True
-       )
-
 #todo: review this code: , separate it, u-test it.
 def remove_vertices_and_faces(verts, faces, nil_areas_whichfaces, map12):
     """ nil_areas_whichfaces: indices of faces that need to be removed.
@@ -1349,11 +1339,13 @@ def vertex_resampling(verts, faceslist_neighbours_of_vertex, face_neighbours_of_
         #mimj[mimj>1.0] = 1.0
         if mimj > 1.0:
             mimj = 1.0
+        if mimj < -1.0:
+            mimj = -1.0
         pipj = np.linalg.norm(pi - pj)
         #print "pipj ", pipj, "  arccos= ",np.arccos(mimj)/np.pi*180 #why is it zero??
         assert pipj == np.linalg.norm(pi - pj, ord=2)
 
-        if pipj==0:
+        if pipj == 0.:
             failure_pairs.append( (i, j) )
             #raise TroubledMesh("repeated triangle")
             pipj = 1
@@ -1361,8 +1353,13 @@ def vertex_resampling(verts, faceslist_neighbours_of_vertex, face_neighbours_of_
         #FIXME
         assert pipj > 0  # fails # ****
 
+        #if (i, j) == (358, 469):
+        #    print "pipj", pipj, "   == %5.20g"%(pipj)
+        #    print np.arccos(mimj)
+        #    print mimj
+        #    #set_trace()
         kij = np.arccos(mimj) / pipj  # radians?
-        assert not np.isnan(kij)  # fails
+        assert not np.isnan(kij), "i=%d,j=%d"%(i, j)  # fails
         return kij
 
     def wi(i_facet, ja_facets, c):
@@ -1512,224 +1509,9 @@ def make_bricks():
     return iobj, RANGE_MIN, RANGE_MAX, STEPSIZE
 
 
-def visualise_gradients(mlab, pos, iobj, arrow_size):
-    lm = arrow_size  # 1.  # STEPSIZE
-    pos4 = np.concatenate((pos, np.ones((pos.shape[0],1))),axis=1)
-    pnormals = - iobj.implicitGradient(pos4)
-    pnormals = normalize_vector4_vectorized(pnormals)
-    check_vector4_vectorized(pos4)
-    xyz = pos
-    uvw = pnormals [:,0:3] / 2.
-    xx, yy, zz = xyz[:, 0], xyz[:, 1], xyz[:, 2]
-    uu, vv, ww = uvw[:, 0], uvw[:, 1], uvw[:, 2]
-    #ax.quiver
-    #ax.quiver(xx, yy, zz,   uu, vv, ww,  length=np.abs(lm), arrow_length_ratio=0.3, alpha=0.3, pivot="tail")
-    #arrow_length_ratio=   length=np.abs(lm)
-    #pivot: tail | middle | tip
-    #mlab.quiver3d(x_verts,y_verts,z_verts, UVW_normals[:,0],UVW_normals[:,1],UVW_normals[:,2],color=(0,0,0))
-    mlab.quiver3d(xx, yy, zz, uu, vv, ww, color=(0, 0, 0), scale_factor=np.abs(lm), line_width=0.5)
 
 
-def visualise_displacements(mlab, verts_from, verts_to):
-    n = verts_from.shape[0]
-    assert verts_from.shape == (n, 3)
-    assert verts_to.shape == (n, 3)
-
-    xyz1 = verts_from
-    xyz2 = verts_to
-    set_trace()
-    ddd = xyz2 - xyz1  #displacement
-    arrow_size = np.mean(np.linalg.norm(ddd, axis=1)) * 0.3
-
-    xx, yy, zz = xyz1[:, 0], xyz1[:, 1], xyz1[:, 2]
-    uu, vv, ww = ddd[:, 0], ddd[:, 1], ddd[:, 2]
-    mlab.quiver3d(xx, yy, zz, uu, vv, ww, color=(0, 0, 0), scale_factor=arrow_size)
-    #line_width=0.5
-
-
-
-def display_simple_using_mayavi_2(vf_list, pointcloud_list, minmax=(-1,1), mayavi_wireframe=False, opacity=1.0,
-        separate_panels=True, gradients_at=None, gradients_from_iobj=None, pointsizes=None, pointcloud_opacity=1.,
-        add_noise=[], noise_added_before_broadcast=False, fromto=None, random_interior_point=False, labels=None):
-    """Two separate panels"""
-
-    print"Mayavi: importing..", ; sys.stdout.flush()
-
-    from mayavi import mlab
-    print "Imported."; sys.stdout.flush()
-
-    if pointsizes is None:
-        pointsizes = [0.2]*10
-
-    if type(opacity) is list:
-        opacities = opacity  # 1.0
-    else:
-        opacities = [opacity] + [0.2]*(len(vf_list)-1)  # 1.0, 0.2 #0.1
-
-    for fi in range(len(vf_list)):
-        if separate_panels:
-            mlab.figure()
-
-        #allpoints are plottedon all panels?
-        color_list = [(1, 0, 0), (0, 0, 0), (1, 1, 0), (0, 0, 1), (0,1,0)]
-        i = 0
-        for c in pointcloud_list:
-            #if separate_panels:
-            #    if i != fi:
-            #        continue
-            mlab.points3d(c[:, 0], c[:, 1], c[:, 2], color=color_list[i], scale_factor=pointsizes[i], opacity=pointcloud_opacity)
-            i += 1
-        del i
-
-        vf = vf_list[fi]
-        verts, faces = vf
-
-        if verts is None:
-            continue
-        if verts.size == 0:
-            print("Warning: empty vertices")
-            continue
-        if faces.size == 0:
-            print("Warning: no faces")
-            continue
-
-        assert verts.ndim == 2
-        assert faces.ndim == 2
-        assert verts.shape == (verts.shape[0], 3), str(verts.shape)
-        assert faces.shape == (faces.shape[0], 3), str(faces.shape)
-        if type(mayavi_wireframe) is list:
-            wire_frame1 = mayavi_wireframe[fi]
-            assert len(mayavi_wireframe) == len(vf_list)
-        else:
-            wire_frame1 = mayavi_wireframe
-        M = 0.1*0
-        if len(add_noise)>0:
-            assert len(add_noise) == len(vf_list)
-            M = float(add_noise[fi])
-
-        ignore_noise = False
-        if ignore_noise:
-            _v = verts
-            _f = faces
-        else:
-            if noise_added_before_broadcast:
-                pre_expand_noise = True
-                post_expand_noise = False
-            else:
-                pre_expand_noise = False
-                post_expand_noise = True
-
-            if pre_expand_noise:
-                print "noise", M/2.
-                verts = noisy(verts, M/2.)
-
-            _v = verts[faces, :]
-            #print _v.shape
-
-            _nv = np.prod(faces.shape)
-            _v = _v.reshape( (_nv, 3) )
-            assert _nv % 3 == 0
-            _f = np.arange(_nv).reshape( (_nv/3, 3) )
-
-            qq = _v[_f,:]
-
-            #print np.nonzero( np.isnan(_f.ravel()) )
-            #print np.nonzero( np.isinf(_f.ravel()) )
-            vv=_f.ravel().copy(); vv.sort()
-            #print np.nonzero(vv != np.arange(vv.size))
-
-            #_v = np.concatenate( (_v, np.zeros( (10000, 3) )), axis=0)
-            if post_expand_noise:
-                #_v = _v + (np.random.rand( _v.shape[0], _v.shape[1] ) -0.5) * M
-                _v = noisy(_v, M/2.)
-                print "noise2 ", M/2.
-
-            #qq = _v[_f, :]
-            #assert  np.all( _f.ravel() == np.arange( _f.size ) )
-            #print qq
-            #print _v
-            #assert np.all(qq.ravel() == _v.ravel())
-
-        #v1 = [_v[0]+M*np.random.rand() for vert in verts],
-        #v2 = [_v[1]+M*np.random.rand() for vert in verts],
-        #v3 = [_v[2]+M*np.random.rand() for vert in verts],
-        v1 = [v[0] for v in _v]
-        v2 = [v[1] for v in _v]
-        v3 = [v[2] for v in _v]
-        mlab.triangular_mesh(
-                        v1, v2, v3, _f,
-                        representation="surface" if not wire_frame1 else "wireframe",
-                        opacity=opacities[fi], scale_factor = 100.0)
-        #opacity = 0.2 #0.1
-
-
-        if minmax is not None:
-            (RANGE_MIN, RANGE_MAX) = minmax
-            x = np.linspace(RANGE_MIN,RANGE_MAX,2).reshape(2,1)
-            y = np.zeros((2,1))
-            z = np.zeros((2,1))
-
-            mlab.plot3d(x, y, z,line_width=3,name="x-axis")
-            mlab.plot3d(y, x, z,line_width=3,name="y-axis")
-            mlab.plot3d(z, y, x,line_width=3,name="z-axis")
-
-            mlab.text3d(RANGE_MAX,0,0, "x", scale=0.3)
-            mlab.text3d(0,RANGE_MAX,0, "y", scale=0.3)
-            mlab.text3d(0,0,RANGE_MAX, "z", scale=0.3)
-            mlab.text3d(RANGE_MIN,0,0, "-x", scale=0.3)
-            mlab.text3d(0,RANGE_MIN,0, "-y", scale=0.3)
-            mlab.text3d(0,0,RANGE_MIN, "-z", scale=0.3)
-
-        visualise_centroid_ids = False
-        if visualise_centroid_ids:
-            for fii in range(faces.shape[0]):
-                if fii in [65, 274, 310, 362]:
-                    f3 = faces[fii, :]
-                    v123 = verts[f3, :]
-                    centroid = np.mean(v123, axis=0)
-                    mlab.text3d(centroid[0],centroid[1],centroid[2], str(fii), scale=0.1 * 0.2)
-
-        if not labels == None:
-            (labels_xyz, labels_ids) = labels
-            for vii in range(labels_xyz.shape[0]):
-                if vii in labels_ids:
-                    v123 = labels_xyz[vii, :]
-                    mlab.text3d(v123[0], v123[1], v123[2], str(vii), scale=0.03)
-
-    def add_random_interior_points(ax, iobj, avg_edge_len):
-        """ Adding random points """
-        n = 10000
-        import basic_types
-        # ******
-        #print avg_edge_len, "WHY USED BEFORE DEFINED?"
-        ampl = avg_edge_len
-        #ampl = 2
-        x = basic_types.make_random_vector_vectorized(n, ampl, 1, type="rand", normalize=False)
-        v = iobj.implicitFunction(x)
-        x_sel =  x[ v >= 0 , :]
-        if x_sel.size ==0:
-            print("No points")
-            return
-        ax.points3d(x_sel[:,0], x_sel[:,1], x_sel[:,2], color=(0,0,0), scale_factor=0.2)
-
-    if gradients_at is not None:
-        verts1, faces1 = vf_list[0]
-        avg_edge_len = compute_average_edge_length(verts, faces)
-        print "avg_edge_len", avg_edge_len
-        visualise_gradients(mlab, gradients_at, gradients_from_iobj, avg_edge_len / 20. *10.)
-    if gradients_from_iobj is not None:
-        if random_interior_point:
-            add_random_interior_points(mlab, gradients_from_iobj, avg_edge_len)
-
-    if not fromto == None:
-        (verts_from, verts_to) = fromto
-        assert verts_from.shape[1] == 3
-        assert verts_to.shape[1] == 3
-        assert verts_to.shape[0] < 10000
-        visualise_displacements(mlab, verts_from, verts_to)
-    mlab.show()
-    return
-
+from visual5 import *
 
 #python -O -m kernprof -v -l ohtake_belyaev_5.py
 #@profile
@@ -2138,9 +1920,6 @@ def propagated_subdiv(facets, subdivided_edges):
     return propag_dict, edges_need_subdivision
 
 
-def noisy(v, ampl):
-    v = v + (np.random.rand(v.shape[0],v.shape[1])*2.-1.) * ampl
-    return v
 
 
 def subdivide_multiple_facets(verts_old, facets_old, tobe_subdivided_face_indices, midpoint_map):
@@ -2764,6 +2543,11 @@ def test_example_meshes():
     #print v2
 
 
+def bigprint(text):
+    print "# "+("*"*52)+"\n# "+text+"\n# "+("*"*52)
+    sys.stdout.flush()
+
+
 import mesh_utils
 
 def demo_everything():
@@ -2776,12 +2560,14 @@ def demo_everything():
     from example_objects import make_example_vectorized
     iobj = make_example_vectorized(
         #"rcube_vec")  #
-        "rdice_vec")  #
+        #"rdice_vec")  #
         #"cube_example") # problem: zero facet areas.  otherwise, it works.
         #"ell_example1")  #+
-        #"bowl_15_holes")  # works too. But too many faces => too slow, too much memory. 32K?
+        "bowl_15_holes")  # works too. But too many faces => too slow, too much memory. 32K?
         #"french_fries_vectorized")
     (RANGE_MIN, RANGE_MAX, STEPSIZE) = (-3, +5, 0.2*1.5/1.5  *2. /2.)
+    #STEPSIZE = STEPSIZE / 2. /1.5
+    #STEPSIZE = STEPSIZE * 2.
 
     #iobj, RANGE_MIN, RANGE_MAX, STEPSIZE = make_bricks()
     #iobj, RANGE_MIN, RANGE_MAX, STEPSIZE = cube_with_cylinders(1)
@@ -2893,6 +2679,7 @@ def demo_everything():
         # *******************************************************
         # PROJECTION
         # *******************************************************
+        bigprint("projection")
 
         #non-vectorized version
         #from ohtake_surface_projection import set_centers_on_surface__ohtake
@@ -2925,13 +2712,13 @@ def demo_everything():
         with Timer() as t2:
             print
             print "2"*100; sys.stdout.flush()
-            set_centers_on_surface__ohtake_v3s(iobj, new_centroids2, average_edge*1., nones_map)
+            set_centers_on_surface__ohtake_v3s(iobj, new_centroids2, average_edge*2., nones_map)
             #new_centroids is the output
             print
             print "---"*100; sys.stdout.flush()
         print("Projection two methods: done within ", t1.interval, t2.interval, "RATIO =", t1.interval/t2.interval)
 
-        print (" Ratio= "+str(t1.interval/t2.interval))*30   # Linter's bug
+        #print (" Ratio= "+str(t1.interval/t2.interval))*30   # Linter's bug
 
         #9 times faster
         #11 times faster: 7.12189273357336, 0.6463103363521201, 'RATIO =', 11.019308114071752)
@@ -2962,6 +2749,7 @@ def demo_everything():
             vertices_apply_qem3(verts, facets, new_centroids, vertex_neighbour_facelist_dict, centroid_gradients, old_centroids_debug=old_centroids)
         #verts = nv1
         #new_verts_qem = verts
+        print "QEM done"
 
         #print collector
         #set_trace()
@@ -3199,7 +2987,7 @@ def vertices_apply_qem3(verts, facets, centroids, vertex_neighbour_facelist_dict
         assert s[0] == np.max(s)
         #print( s / s[0] )  # [  1.00000000e+00   1.13351623e-16   6.85747820e-52]
 
-        tau = 10. ** 3.
+        tau = 1000.
         s[s / s[0] < 1.0/tau] = 0
         #print(s , s[0] , tau)
         rank = np.sum(s / s[0] > 1.0/tau)
---
+++ implicit/visual5.py
@@ -0,0 +1,238 @@
+import numpy as np
+
+import sys
+print"Mayavi: importing..", ; sys.stdout.flush()
+from mayavi import mlab
+print "Imported."; sys.stdout.flush()
+
+def noisy(v, ampl):
+    v = v + (np.random.rand(v.shape[0],v.shape[1])*2.-1.) * ampl
+    return v
+
+def visualise_gradients(mlab, pos, iobj, arrow_size):
+    lm = arrow_size  # 1.  # STEPSIZE
+    pos4 = np.concatenate((pos, np.ones((pos.shape[0],1))),axis=1)
+    pnormals = - iobj.implicitGradient(pos4)
+    pnormals = normalize_vector4_vectorized(pnormals)
+    check_vector4_vectorized(pos4)
+    xyz = pos
+    uvw = pnormals [:,0:3] / 2.
+    xx, yy, zz = xyz[:, 0], xyz[:, 1], xyz[:, 2]
+    uu, vv, ww = uvw[:, 0], uvw[:, 1], uvw[:, 2]
+    #ax.quiver
+    #ax.quiver(xx, yy, zz,   uu, vv, ww,  length=np.abs(lm), arrow_length_ratio=0.3, alpha=0.3, pivot="tail")
+    #arrow_length_ratio=   length=np.abs(lm)
+    #pivot: tail | middle | tip
+    #mlab.quiver3d(x_verts,y_verts,z_verts, UVW_normals[:,0],UVW_normals[:,1],UVW_normals[:,2],color=(0,0,0))
+    mlab.quiver3d(xx, yy, zz, uu, vv, ww, color=(0, 0, 0), scale_factor=np.abs(lm), line_width=0.5)
+
+
+def visualise_displacements(mlab, verts_from, verts_to):
+    n = verts_from.shape[0]
+    assert verts_from.shape == (n, 3)
+    assert verts_to.shape == (n, 3)
+
+    xyz1 = verts_from
+    xyz2 = verts_to
+    set_trace()
+    ddd = xyz2 - xyz1  #displacement
+    arrow_size = np.mean(np.linalg.norm(ddd, axis=1)) * 0.3
+
+    xx, yy, zz = xyz1[:, 0], xyz1[:, 1], xyz1[:, 2]
+    uu, vv, ww = ddd[:, 0], ddd[:, 1], ddd[:, 2]
+    mlab.quiver3d(xx, yy, zz, uu, vv, ww, color=(0, 0, 0), scale_factor=arrow_size)
+    #line_width=0.5
+
+
+def display_simple_using_mayavi_2(vf_list, pointcloud_list, minmax=(-1,1), mayavi_wireframe=False, opacity=1.0,
+        separate_panels=True, gradients_at=None, gradients_from_iobj=None, pointsizes=None, pointcloud_opacity=1.,
+        add_noise=[], noise_added_before_broadcast=False, fromto=None, random_interior_point=False, labels=None):
+    """Two separate panels"""
+
+    #print"Mayavi: importing..", ; sys.stdout.flush()
+
+    #from mayavi import mlab
+    #print "Imported."; sys.stdout.flush()
+
+    if pointsizes is None:
+        pointsizes = [0.2]*10
+
+    if type(opacity) is list:
+        opacities = opacity  # 1.0
+    else:
+        opacities = [opacity] + [0.2]*(len(vf_list)-1)  # 1.0, 0.2 #0.1
+
+    for fi in range(len(vf_list)):
+        if separate_panels:
+            mlab.figure()
+
+        #allpoints are plottedon all panels?
+        color_list = [(1, 0, 0), (0, 0, 0), (1, 1, 0), (0, 0, 1), (0,1,0)]
+        i = 0
+        for c in pointcloud_list:
+            #if separate_panels:
+            #    if i != fi:
+            #        continue
+            mlab.points3d(c[:, 0], c[:, 1], c[:, 2], color=color_list[i], scale_factor=pointsizes[i], opacity=pointcloud_opacity)
+            i += 1
+        del i
+
+        vf = vf_list[fi]
+        verts, faces = vf
+
+        if verts is None:
+            continue
+        if verts.size == 0:
+            print("Warning: empty vertices")
+            continue
+        if faces.size == 0:
+            print("Warning: no faces")
+            continue
+
+        assert verts.ndim == 2
+        assert faces.ndim == 2
+        assert verts.shape == (verts.shape[0], 3), str(verts.shape)
+        assert faces.shape == (faces.shape[0], 3), str(faces.shape)
+        if type(mayavi_wireframe) is list:
+            wire_frame1 = mayavi_wireframe[fi]
+            assert len(mayavi_wireframe) == len(vf_list)
+        else:
+            wire_frame1 = mayavi_wireframe
+        M = 0.1*0
+        if len(add_noise)>0:
+            assert len(add_noise) == len(vf_list)
+            M = float(add_noise[fi])
+
+        ignore_noise = False
+        if ignore_noise:
+            _v = verts
+            _f = faces
+        else:
+            if noise_added_before_broadcast:
+                pre_expand_noise = True
+                post_expand_noise = False
+            else:
+                pre_expand_noise = False
+                post_expand_noise = True
+
+            if pre_expand_noise:
+                print "noise", M/2.
+                verts = noisy(verts, M/2.)
+
+            _v = verts[faces, :]
+            #print _v.shape
+
+            _nv = np.prod(faces.shape)
+            _v = _v.reshape( (_nv, 3) )
+            assert _nv % 3 == 0
+            _f = np.arange(_nv).reshape( (_nv/3, 3) )
+
+            qq = _v[_f,:]
+
+            #print np.nonzero( np.isnan(_f.ravel()) )
+            #print np.nonzero( np.isinf(_f.ravel()) )
+            vv=_f.ravel().copy(); vv.sort()
+            #print np.nonzero(vv != np.arange(vv.size))
+
+            #_v = np.concatenate( (_v, np.zeros( (10000, 3) )), axis=0)
+            if post_expand_noise:
+                #_v = _v + (np.random.rand( _v.shape[0], _v.shape[1] ) -0.5) * M
+                _v = noisy(_v, M/2.)
+                print "noise2 ", M/2.
+
+            #qq = _v[_f, :]
+            #assert  np.all( _f.ravel() == np.arange( _f.size ) )
+            #print qq
+            #print _v
+            #assert np.all(qq.ravel() == _v.ravel())
+
+        #v1 = [_v[0]+M*np.random.rand() for vert in verts],
+        #v2 = [_v[1]+M*np.random.rand() for vert in verts],
+        #v3 = [_v[2]+M*np.random.rand() for vert in verts],
+        v1 = [v[0] for v in _v]
+        v2 = [v[1] for v in _v]
+        v3 = [v[2] for v in _v]
+        mlab.triangular_mesh(
+                        v1, v2, v3, _f,
+                        representation="surface" if not wire_frame1 else "wireframe",
+                        opacity=opacities[fi], scale_factor = 100.0)
+        #opacity = 0.2 #0.1
+
+
+        if minmax is not None:
+            (RANGE_MIN, RANGE_MAX) = minmax
+            x = np.linspace(RANGE_MIN,RANGE_MAX,2).reshape(2,1)
+            y = np.zeros((2,1))
+            z = np.zeros((2,1))
+
+            mlab.plot3d(x, y, z,line_width=3,name="x-axis")
+            mlab.plot3d(y, x, z,line_width=3,name="y-axis")
+            mlab.plot3d(z, y, x,line_width=3,name="z-axis")
+
+            mlab.text3d(RANGE_MAX,0,0, "x", scale=0.3)
+            mlab.text3d(0,RANGE_MAX,0, "y", scale=0.3)
+            mlab.text3d(0,0,RANGE_MAX, "z", scale=0.3)
+            mlab.text3d(RANGE_MIN,0,0, "-x", scale=0.3)
+            mlab.text3d(0,RANGE_MIN,0, "-y", scale=0.3)
+            mlab.text3d(0,0,RANGE_MIN, "-z", scale=0.3)
+
+        visualise_centroid_ids = False
+        if visualise_centroid_ids:
+            for fii in range(faces.shape[0]):
+                if fii in [65, 274, 310, 362]:
+                    f3 = faces[fii, :]
+                    v123 = verts[f3, :]
+                    centroid = np.mean(v123, axis=0)
+                    mlab.text3d(centroid[0],centroid[1],centroid[2], str(fii), scale=0.1 * 0.2)
+
+        if not labels == None:
+            (labels_xyz, labels_ids) = labels
+            for vii in range(labels_xyz.shape[0]):
+                if vii in labels_ids:
+                    v123 = labels_xyz[vii, :]
+                    mlab.text3d(v123[0], v123[1], v123[2], str(vii), scale=0.03)
+
+    def add_random_interior_points(ax, iobj, avg_edge_len):
+        """ Adding random points """
+        n = 10000
+        import basic_types
+        # ******
+        #print avg_edge_len, "WHY USED BEFORE DEFINED?"
+        ampl = avg_edge_len
+        #ampl = 2
+        x = basic_types.make_random_vector_vectorized(n, ampl, 1, type="rand", normalize=False)
+        v = iobj.implicitFunction(x)
+        x_sel =  x[ v >= 0 , :]
+        if x_sel.size ==0:
+            print("No points")
+            return
+        ax.points3d(x_sel[:,0], x_sel[:,1], x_sel[:,2], color=(0,0,0), scale_factor=0.2)
+
+    if gradients_at is not None:
+        verts1, faces1 = vf_list[0]
+        avg_edge_len = compute_average_edge_length(verts, faces)
+        print "avg_edge_len", avg_edge_len
+        visualise_gradients(mlab, gradients_at, gradients_from_iobj, avg_edge_len / 20. *10.)
+    if gradients_from_iobj is not None:
+        if random_interior_point:
+            add_random_interior_points(mlab, gradients_from_iobj, avg_edge_len)
+
+    if not fromto == None:
+        (verts_from, verts_to) = fromto
+        assert verts_from.shape[1] == 3
+        assert verts_to.shape[1] == 3
+        assert verts_to.shape[0] < 10000
+        visualise_displacements(mlab, verts_from, verts_to)
+    mlab.show()
+    return
+
+
+def quick_vis(verts, facets, face_idx):
+    #print facets.shape
+    #print facets[face_idx, :].shape
+    display_simple_using_mayavi_2( [(verts, facets), (verts, facets[face_idx, :]), (verts, facets[face_idx, :])],
+       pointcloud_list=[ ], pointcloud_opacity=0.2,
+       mayavi_wireframe=[True, True, True], opacity=[0.1, 1, 0.2],
+       gradients_at=None, separate_panels=False, gradients_from_iobj=None, add_noise=[0, 0, 0.01],
+       noise_added_before_broadcast=True
+       )
