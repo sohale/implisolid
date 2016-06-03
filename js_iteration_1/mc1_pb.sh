287a9347367eacb1c5c15cb17ab04494800d8bfc
Michael Karotsieris <michael.karotsieris@gmail.com>
2016-06-03 15:04:29 +0100

    added script for C++ --> JS transpile

---
+++ js_iteration_1/mc1_pb.sh
@@ -0,0 +1,7 @@
+
+
+
+# em++ -I ./boost_1_61_0/ -s EXPORTED_FUNCTIONS="['_main']" -pedantic -std=c++14 -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS -O3 vect_test_2.cpp -o vect_test_2.js
+
+  ~/emsdk_portable/emscripten/master/em++ -I /usr/local/include/boost_1_61_0/  -s EXPORTED_FUNCTIONS="['_make_object']"  -pedantic -std=c++14  mc1_pb.cpp  -o mc1_pb.js && echo DONE
+   # -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS \
