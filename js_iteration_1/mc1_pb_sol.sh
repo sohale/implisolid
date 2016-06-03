8baee3835ac64dc590e86d1853be445818150140
schauvier <s.chauvier@gmail.com>
2016-06-03 18:11:13 +0100

    Algo with Eigen

--- js_iteration_1/mc1_pb_sol.sh
+++ js_iteration_1/mc1_pb_sol.sh
@@ -3,5 +3,7 @@
 
 # em++ -I ./boost_1_61_0/ -s EXPORTED_FUNCTIONS="['_main']" -pedantic -std=c++14 -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS -O3 vect_test_2.cpp -o vect_test_2.js
 
-  ~/Desktop/emsdk_portable/emscripten/master/em++ -I /usr/local/include/boost_1_61_0/  -s EXPORTED_FUNCTIONS="['_make_object']"  -pedantic -std=c++14  mc1_pb.cpp  -o mc1_pb.js && echo DONE
+  # ~/Desktop/emsdk_portable/emscripten/master/em++ -I /usr/local/include/boost_1_61_0/  -s EXPORTED_FUNCTIONS="['_make_object']"  -pedantic -std=c++14  mc1_pb.cpp  -o mc1_pb.js && echo DONE
    # -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS \
+   ~/Desktop/emsdk_portable/emscripten/master/em++ -I /usr/local/include/Eigen/  -s EXPORTED_FUNCTIONS="['_make_object']"  -pedantic -std=c++14  mc1_eigen.cpp  -o mc1_eigen.js && echo DONE
+    # -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS \
