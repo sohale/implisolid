2b87f25dbe36ac1aa592cb3efa677c5628f21f90
sohale <sohale@gmail.com>
2016-06-06 12:54:09 +0100

    compiled file renamed x.js -> x.cpp.js

--- js_iteration_1/mc2_pb.sh
+++ js_iteration_1/mc2_pb.sh
@@ -1,5 +1,5 @@
 echo "@rem em++ -I /usr/local/inlcude/boost_1_61_0/ -s EXPORTED_FUNCTIONS="['_main']" -pedantic -std=c++14  -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS  -O3     vect_test_2.cpp    -o vect_test_2.js"
-  ~/emsdk_portable/emscripten/master/em++ -I /usr/local/include/boost_1_61_0/ -s EXPORTED_FUNCTIONS="['_make_object', '_main']" -s NO_EXIT_RUNTIME=1 -s ASSERTIONS=1 -pedantic -std=c++14 mc2_pb.cpp -o mc2_pb.js
+  ~/emsdk_portable/emscripten/master/em++ -I /usr/local/include/boost_1_61_0/ -s EXPORTED_FUNCTIONS="['_make_object', '_main']" -s NO_EXIT_RUNTIME=1 -s ASSERTIONS=1 -pedantic -std=c++14 mc2_pb.cpp -o mc2_pb.cpp.js
 
 # ::     -s EXPORTED_FUNCTIONS="['_main', '_make_object']" ^
 # ::     -s EXPORTED_FUNCTIONS="['_main']" ^
