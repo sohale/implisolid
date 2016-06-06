e57b2c75287e659cd78e5e4e90cf33fd18308a0a
Michael Karotsieris <michael.karotsieris@gmail.com>
2016-06-06 12:08:19 +0100

    sh script for compilation of mc2_pb

---
+++ js_iteration_1/mc2_pb.sh
@@ -0,0 +1,16 @@
+echo "@rem em++ -I /usr/local/inlcude/boost_1_61_0/ -s EXPORTED_FUNCTIONS="['_main']" -pedantic -std=c++14  -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS  -O3     vect_test_2.cpp    -o vect_test_2.js"
+  ~/emsdk_portable/emscripten/master/em++ -I /usr/local/include/boost_1_61_0/ -s EXPORTED_FUNCTIONS="['_make_object', '_main']" -s NO_EXIT_RUNTIME=1 -s ASSERTIONS=1 -pedantic -std=c++14 mc2_pb.cpp -o mc2_pb.js
+
+# ::     -s EXPORTED_FUNCTIONS="['_main', '_make_object']" ^
+# ::     -s EXPORTED_FUNCTIONS="['_main']" ^
+# :: @rem     -O3   ^
+# :: @rem     -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS  ^
+#
+#
+# ::     -O3   ^
+# ::     -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS  ^
+#
+#
+# ::        -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS  ^
+# ::        -O3                           ^
+#
