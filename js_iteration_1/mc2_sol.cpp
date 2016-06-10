8d17c21de9fb1b50a8cead88ed29bbf97b8df400
schauvier <s.chauvier@gmail.com>
2016-06-10 14:45:08 +0100

    Reindexing worksgit pull

--- js_iteration_1/mc2_sol.cpp
+++ js_iteration_1/mc2_sol.cpp
@@ -1027,12 +1027,12 @@ vf_t reindexing_verts_faces(vf_t const& vf)
     }
 
   //  cout << "number of unique verts" << nv_new << endl;
-    boost::array<int, 2> v_shape = {{ nv_new, 3 }};
+    boost::array<int, 2> v_shape = {{ nv_new+3, 3 }};
     boost::multi_array<REAL, 2> verts_new(v_shape);
     // verts_new[0][0] = verts[0][0];
     // verts_new[0][1] = verts[0][1];
     // verts_new[0][2] = verts[0][2];
-    int nv_new_2 = 0;
+    int nv_new_2 = 3;
     for(int i=0; i<nf-1; i++){
       for(int j=0; j<3; j++){
 
@@ -1051,7 +1051,7 @@ vf_t reindexing_verts_faces(vf_t const& vf)
         }
         else {
       //    cout << "Patate " << verts_new.shape()[0] << "nv_new_2 " << nv_new_2  <<  endl;
-        //  assert (nv_new_2 <= verts_new.shape()[0]);
+          assert (nv_new_2 <= verts_new.shape()[0]);
           verts_new[nv_new_2][0] = verts[3*i+j][0];
           verts_new[nv_new_2][1] = verts[3*i+j][1];
           verts_new[nv_new_2][2] = verts[3*i+j][2];
@@ -1060,10 +1060,16 @@ vf_t reindexing_verts_faces(vf_t const& vf)
       }
 
     }
-
+    // verts initialization
     verts_new[0][0] = verts[0][0];
     verts_new[0][1] = verts[0][1];
     verts_new[0][2] = verts[0][2];
+    verts_new[1][0] = verts[1][0];
+    verts_new[1][1] = verts[1][1];
+    verts_new[1][2] = verts[1][2];
+    verts_new[2][0] = verts[2][0];
+    verts_new[2][1] = verts[2][1];
+    verts_new[2][2] = verts[2][2];
     cout << "Patate " << verts_new.shape()[0] << "nv_new_2 " << nv_new_2  <<  endl;
     vf_t p2 = make_pair(verts_new, faces_new);
     return p2;
