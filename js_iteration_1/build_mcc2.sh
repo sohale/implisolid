7709ed678cd0cf7973ca01bcd14836f9cd4149a1
schauvier <s.chauvier@gmail.com>
2016-06-23 11:52:05 +0100

    merging

--- js_iteration_1/build_mcc2.sh
+++ js_iteration_1/build_mcc2.sh
@@ -1,5 +1,43 @@
-echo "@rem em++ -I /usr/local/inlcude/boost_1_61_0/ -s EXPORTED_FUNCTIONS="['_main']" -pedantic -std=c++14  -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS  -O3     vect_test_2.cpp    -o vect_test_2.js"
-  ~/emsdk_portable/emscripten/master/em++ -I /usr/local/include/boost_1_61_0/  -s TOTAL_MEMORY=30100100 -s EXPORTED_FUNCTIONS="['_produce_object_old2', '_main', '_build_geometry', '_get_v_size', '_get_f_size', '_get_f', '_get_v', '_finish_geometry', '_get_f_ptr', '_get_v_ptr']" -s NO_EXIT_RUNTIME=1 -s ASSERTIONS=1 -s DEMANGLE_SUPPORT=1 -pedantic -std=c++14 mcc2.cpp -o mcc2.cpp.js
+#!/bin/bash
+
+# echo "@rem em++ -I /usr/local/inlcude/boost_1_61_0/ -s EXPORTED_FUNCTIONS="['_main']" -pedantic -std=c++14  -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS  -O3     vect_test_2.cpp    -o vect_test_2.js"
+
+BOOST_FOLDER=./boost_1_61_0/
+OPTIM=1
+if [ $OPTIM -eq 0 ]
+then
+
+    echo "** dev version **"
+
+    em++ -I $BOOST_FOLDER  \
+        -s TOTAL_MEMORY=30100100 \
+        -s EXPORTED_FUNCTIONS="['_produce_object_old2', '_main', '_build_geometry', '_get_v_size', '_get_f_size', '_get_f', '_get_v', '_finish_geometry', '_get_f_ptr', '_get_v_ptr']" \
+        -s NO_EXIT_RUNTIME=1 \
+        -s DEMANGLE_SUPPORT=1 \
+         -s ASSERTIONS=1 \
+        -pedantic -std=c++14 \
+        mcc2.cpp -o mcc2.cpp.js
+
+fi
+
+
+if [ $OPTIM -eq 1 ]
+then
+    echo "** optimised version **"
+
+    em++ \
+            -I $BOOST_FOLDER   \
+            -O3   \
+            -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS  \
+            -s EXPORTED_FUNCTIONS="['_produce_object_old2', '_main', '_build_geometry', '_get_v_size', '_get_f_size', '_get_f', '_get_v', '_finish_geometry']" \
+            -s NO_EXIT_RUNTIME=1          \
+            --llvm-lto 1     \
+            -Winline         \
+            -s TOTAL_MEMORY=30100100    \
+            -pedantic -std=c++14  \
+        mcc2.cpp  \
+            -o  mcc2.cpp.js
+fi
 
 # ::     -s EXPORTED_FUNCTIONS="['_main', '_make_object']" ^
 # ::     -s EXPORTED_FUNCTIONS="['_main']" ^1
