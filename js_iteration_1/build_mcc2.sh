4c0b798a3004990d1f6b723638be5af4f286a741
Michael Karotsieris <michael.karotsieris@gmail.com>
2016-06-23 14:51:53 +0100

    added exported functions

--- js_iteration_1/build_mcc2.sh
+++ js_iteration_1/build_mcc2.sh
@@ -2,14 +2,14 @@
 
 # echo "@rem em++ -I /usr/local/inlcude/boost_1_61_0/ -s EXPORTED_FUNCTIONS="['_main']" -pedantic -std=c++14  -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS  -O3     vect_test_2.cpp    -o vect_test_2.js"
 
-BOOST_FOLDER=./boost_1_61_0/
+BOOST_FOLDER=/usr/local/include/boost_1_61_0
 OPTIM=1
 if [ $OPTIM -eq 0 ]
 then
 
     echo "** dev version **"
 
-    em++ -I $BOOST_FOLDER  \
+    ~/emsdk_portable/emscripten/master/em++ -I $BOOST_FOLDER  \
         -s TOTAL_MEMORY=30100100 \
         -s EXPORTED_FUNCTIONS="['_produce_object_old2', '_main', '_build_geometry', '_get_v_size', '_get_f_size', '_get_f', '_get_v', '_finish_geometry', '_get_f_ptr', '_get_v_ptr']" \
         -s NO_EXIT_RUNTIME=1 \
@@ -25,13 +25,12 @@ if [ $OPTIM -eq 1 ]
 then
     echo "** optimised version **"
 
-    em++ \
+    ~/emsdk_portable/emscripten/master/em++ \
             -I $BOOST_FOLDER   \
             -O3   \
             -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS  \
-            -s EXPORTED_FUNCTIONS="['_produce_object_old2', '_main', '_build_geometry', '_get_v_size', '_get_f_size', '_get_f', '_get_v', '_finish_geometry']" \
+            -s EXPORTED_FUNCTIONS="['_produce_object_old2', '_main', '_build_geometry', '_get_v_size', '_get_f_size', '_get_f', '_get_v', '_finish_geometry', '_get_f_ptr', '_get_v_ptr']" \
             -s NO_EXIT_RUNTIME=1          \
-            --llvm-lto 1     \
             -Winline         \
             -s TOTAL_MEMORY=30100100    \
             -pedantic -std=c++14  \
