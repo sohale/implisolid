867ca016d9d8c620f3f35d14113e2722fc5c013f
Michael Karotsieris <michael.karotsieris@gmail.com>
2016-06-23 12:28:01 +0100

    comment out correction

--- js_iteration_1/build_mcc2.sh
+++ js_iteration_1/build_mcc2.sh
@@ -39,6 +39,11 @@ then
             -o  mcc2.cpp.js
 fi
 
+# copy the path for em++
+# echo "@rem em++ -I /usr/local/inlcude/boost_1_61_0/ -s EXPORTED_FUNCTIONS="['_main']" -pedantic -std=c++14  -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS  -O3     vect_test_2.cpp    -o vect_test_2.js"
+#   ~/emsdk_portable/emscripten/master/em++ -I /usr/local/include/boost_1_61_0/  \
+#   -s TOTAL_MEMORY=30100100 -s EXPORTED_FUNCTIONS="['_produce_object_old2', '_main', '_build_geometry', '_get_v_size', '_get_f_size', '_get_f', '_get_v', '_finish_geometry', '_get_f_ptr', '_get_v_ptr']" -O3 -s NO_EXIT_RUNTIME=1 -s ASSERTIONS=1 -s DEMANGLE_SUPPORT=1 -pedantic -std=c++14 mcc2.cpp -o mcc2.cpp.js
+
 # ::     -s EXPORTED_FUNCTIONS="['_main', '_make_object']" ^
 # ::     -s EXPORTED_FUNCTIONS="['_main']" ^1
 # :: @rem     -O3   ^
