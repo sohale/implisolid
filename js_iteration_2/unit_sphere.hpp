34a3fc86cbbccf14a589715f0414f45045685d2d
schauvier <s.chauvier@gmail.com>
2016-06-27 16:12:51 +0100

    Changing the structure of the class

--- js_iteration_2/unit_sphere.hpp
+++ js_iteration_2/unit_sphere.hpp
@@ -11,7 +11,7 @@ public:
     //boost::array<int, 2> big_shape = {{ 10000, 3 }};
     //boost::multi_array<REAL, 2> huge_test =  boost::multi_array<REAL, 2>(big_shape);
 
-    virtual void eval_implicit(const vectorized_vect& x, vectorized_scalar* f_output){
+    virtual void eval_implicit(const vectorized_vect& x, vectorized_scalar& f_output){
         my_assert(assert_implicit_function_io(x, f_output), "");
         my_assert(this->integrity_invariant(), "");
 
@@ -23,19 +23,19 @@ public:
         auto e = x.end();
         for(; i<e; i++, output_ctr++){
             //f_output[output_ctr] = (*x)[0];
-            (*f_output)[output_ctr] = r2 - norm_squared((*i)[0], (*i)[1], (*i)[2] );
+            (f_output)[output_ctr] = r2 - norm_squared((*i)[0], (*i)[1], (*i)[2] );
         }
     }
-    virtual void eval_gradient(const vectorized_vect& x, vectorized_vect* output){
+    virtual void eval_gradient(const vectorized_vect& x, vectorized_vect& output){
         //(*output) = x;
 
         int output_ctr=0;
         auto i = x.begin();
         auto e = x.end();
         for(; i<e; i++, output_ctr++){
-            (*output)[output_ctr][0] = -2. * (*i)[0];
-            (*output)[output_ctr][1] = -2. * (*i)[1];
-            (*output)[output_ctr][2] = -2. * (*i)[2];
+            (output)[output_ctr][0] = -2. * (*i)[0];
+            (output)[output_ctr][1] = -2. * (*i)[1];
+            (output)[output_ctr][2] = -2. * (*i)[2];
         }
     }
     bool integrity_invariant(){
