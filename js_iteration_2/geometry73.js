d861ee596c9613e432f37e3ab16177b0798fac25
sohale <sohale@gmail.com>
2016-06-27 15:43:59 +0100

    IMPLICIT added

---
+++ js_iteration_2/geometry73.js
@@ -0,0 +1,129 @@
+'use strict';
+
+
+
+
+function __check_TypedArray_type(src, _type){
+    //if(typeof src !== type_name)
+    if(!(src instanceof _type))
+     {
+        console.error();
+        throw "Developer's error; " + (typeof src) + (Object.prototype.toString.call(src.buffer));
+    }
+}
+
+// See  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Typed_arrays
+
+function copy_Float32Array_preallocated(src, prealloc_size, randomize)  {
+    __check_TypedArray_type(src, Float32Array);
+    var TYPE_SIZE = 4;
+    var len_bytes = Math.max(prealloc_size*TYPE_SIZE, src.byteLength);
+    var dst = new ArrayBuffer(len_bytes);
+    var r = new Float32Array(dst);
+    r.set(new Float32Array(src));
+    if(randomize){
+        for(var i=0*src.byteLength/TYPE_SIZE;i<r.length;i++){
+            r[i] = (Math.random()-0.5);
+        }
+    }
+    return r;
+}
+function copy_Uint32Array_preallocated(src, prealloc_size)  {
+    __check_TypedArray_type(src, Uint32Array);
+    var TYPE_SIZE = 4;
+    var len_bytes = Math.max(prealloc_size*TYPE_SIZE, src.byteLength);
+    var dst = new ArrayBuffer(len_bytes);
+    var r = new Uint32Array(dst);
+    r.set(new Uint32Array(src));
+    return r;
+}
+
+
+/** Simply creates a geometry . This is static and cannot be modified when displayed. Instantiate a new one and make a new THREE.Mesh() */
+function LiveBufferGeometry73( verts, faces,  pre_allocate) {
+
+    THREE.BufferGeometry.call( this );
+    this.type = 'LiveBufferGeometry73';
+
+    this.parameters = { };
+
+    this.allocate_buffers = function(){
+
+        if(pre_allocate){
+            console.log("Aloocating separate space for verts,faces.");
+            faces = copy_Uint32Array_preallocated(faces, 30000*3);
+            verts = copy_Float32Array_preallocated(verts, 30000*3);
+        }
+        assert(pre_allocate);  // Will be wrong if pre_allocate is false
+
+        // build geometry
+        this.addAttribute( 'position', new THREE.BufferAttribute( verts, 3 ) );
+        this.setIndex( new THREE.BufferAttribute( faces, 3 ) ); //new Uint32Array(faces) ??
+        for(var j=0;j<faces.length;j++)
+            if(this.index.array[j] !== faces[j]){
+                console.error(j);break;
+            }
+        //my_assert(this.index.array === faces);
+
+        if(pre_allocate){
+            console.log("Aloocating separate space for norms, colors.");
+            var normals = copy_Float32Array_preallocated(verts, 30000*3/10000, true);
+            var colors = copy_Float32Array_preallocated(verts, 30000*3/10000, true);
+            //var uvs = copy_Float32Array_preallocated(new Float32Array([]), 30000*3 * 0, true);
+        }
+
+        this.addAttribute( 'normal', new THREE.BufferAttribute( normals, 3, true ) );
+        this.addAttribute( 'color', new THREE.BufferAttribute( colors, 3, true ) );
+        //this.addAttribute( 'uv', new THREE.BufferAttribute( uvs, 2 ) );
+
+        var materialIndex = 0;
+        this.addGroup( 0, faces.length*1-10, materialIndex );
+    }
+
+    // ThreeJS does not use prototype-based OOP.
+
+    this.update_geometry = function(implicit_service) {
+        var geometry = this;
+        var nverts = implicit_service.get_v_size();
+        var nfaces = implicit_service.get_f_size();
+        var verts_address = implicit_service.get_v_ptr();
+        var faces_address = implicit_service.get_f_ptr();
+        var verts = Module.HEAPF32.subarray(
+            verts_address/_FLOAT_SIZE,
+            verts_address/_FLOAT_SIZE + 3*nverts);
+        var faces = Module.HEAPU32.subarray(
+            faces_address/_INT_SIZE,
+            faces_address/_INT_SIZE + 3*nfaces);
+
+        var g_nverts = geometry.attributes.position.array.length/3;  // Physical space size.
+        var g_nfaces = geometry.index.array.length/3;
+
+        var nv3 = Math.min(nverts, g_nverts) * 3;
+        var nf3 = Math.min(nfaces, g_nfaces) * 3;
+        my_assert(nv3 === verts.length);
+        my_assert(nf3 === faces.length);
+
+        // *************************************
+        // * The following code works only when we use a single instance of the geometry, i.e. used in one Mesh.
+        // * It will not work well if the same BufferGeometry is used in more than one Mesh
+        // * So the object will look fine if we disable the wireframe mesh.
+        // *************************************
+        geometry.attributes.position.array.set(verts);
+        geometry.index.array.set(faces);
+
+
+        geometry.setDrawRange( 0, nf3 );
+        /*geometry.clearGroups();
+        geometry.addGroup( 0, nf3, 0 );
+        */
+
+        geometry.attributes.position.needsUpdate = true;
+        geometry.index.needsUpdate = true;
+    };
+
+    this.allocate_buffers()
+};
+
+LiveBufferGeometry73.prototype = Object.create( THREE.BufferGeometry.prototype );
+LiveBufferGeometry73.prototype.constructor = LiveBufferGeometry73;
+
---
+++ js_iteration_2/implisolid_main.js
@@ -0,0 +1,86 @@
+'use strict';
+
+
+function init(service) {
+    'use strict';
+    //main = Module.cwrap('main', 'number', []);
+    //var service={}; //= newProducer //is an interface
+    service.build_geometry = Module.cwrap('build_geometry', null, ['number', 'number']);
+    service.get_v_size = Module.cwrap('get_v_size', 'number', []);
+    service.get_f_size = Module.cwrap('get_f_size', 'number', []);
+    service.get_v = Module.cwrap('get_v', null, ['number']);
+    service.get_f = Module.cwrap('get_f', null, ['number']);
+    service.get_v_ptr = Module.cwrap('get_v_ptr', 'number', []);
+    service.get_f_ptr = Module.cwrap('get_f_ptr', 'number', []);
+    service.finish_geometry = Module.cwrap('finish_geometry', null, []);
+
+    service.init = function(){ service.needsFinish = false; }
+    service.finish_with = function (){
+        //after the last round.
+        if(!this.needsFinish){
+            console.error("cannot `finish_geometry()`. Geometry not produced.");
+        }
+        service.finish_geometry();
+        service.needsFinish = false;
+    }
+
+    service.init();
+    return service;
+}
+
+
+var ImplicitService = function(){
+    init(this);
+    this.make_geometry = function (params) {
+        var startTime = new Date();
+
+        if(this.needsFinish) {
+            this.finish_geometry();
+            this.needsFinish = false;
+        }
+        this.build_geometry( 28, params["subjective_time"]);
+        this.needsFinish = true;
+
+        var nverts = this.get_v_size();
+        var nfaces = this.get_f_size();
+
+        var verts_address = this.get_v_ptr();
+        var faces_address = this.get_f_ptr();
+
+        var verts = Module.HEAPF32.subarray(verts_address/_FLOAT_SIZE, verts_address/_FLOAT_SIZE + 3*nverts);
+        var faces = Module.HEAPU32.subarray(faces_address/_INT_SIZE, faces_address/_INT_SIZE + 3*nfaces);
+
+        var allocate_buffers = true;
+        var geom = new LiveBufferGeometry73(verts, faces, allocate_buffers);
+
+        var endTime = new Date();
+        var timeDiff = endTime - startTime;
+
+        //report_time(timeDiff, function(){hist();});
+
+        return geom;
+    };
+
+    this.getLiveGeometry = function(){
+        var geom = this.make_geometry( {subjective_time: 0.0} );
+        return geom;
+    }
+
+};
+
+var IMPLICIT = null;
+function _on_cpp_loaded() {
+    console.log("C++ ready.");
+    //IMPLISOLID.
+    IMPLICIT = IMPLICITService();
+};
+
+
+/* Put hte following in the HTML
+<script>
+        Module={preRun:[],
+        onRuntimeInitialized: _on_cpp_loaded,
+    };
+</script>
+<script type="text/javascript" src="mcc2.cpp.js"></script>
+*/
