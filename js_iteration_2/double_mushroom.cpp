a5da0941d0f6e62f49ae4267a737fb0759a4c3b3
marcFraysse <fraysse.marc@gmail.com>
2016-06-29 11:00:57 +0100

    working vertex resampling for the double mushroom

--- js_iteration_2/double_mushroom.cpp
+++ js_iteration_2/double_mushroom.cpp
@@ -24,29 +24,33 @@ public:
         auto e = x.end();
         for(; i<e; i++, output_ctr++){
             (f_output)[output_ctr] = -(pow((*i)[0],2)*r2+pow((*i)[1],2)*r2-pow((*i)[2],2)*r2-1);
-            if((*i)[2]==0.8 ) (f_output)[output_ctr]=1;
             if((*i)[2]>0.8) (f_output)[output_ctr]=-1;
-            if((*i)[2]==-0.8 ) (f_output)[output_ctr]=1;
             if((*i)[2]<-0.8) (f_output)[output_ctr]=-1;
 
         }
     }
     void eval_gradient(const vectorized_vect& x, vectorized_vect& output){
         //(*output) = x;
-
+        const REAL r2 = squared(this->r);
         int output_ctr=0;
         auto i = x.begin();
         auto e = x.end();
         for(; i<e; i++, output_ctr++){
-            (output)[output_ctr][0] = 2. * (*i)[0];
-            (output)[output_ctr][1] = 2. * (*i)[1];
-            (output)[output_ctr][2] = -2. * (*i)[2];
-            if((*i)[2]>0.79 && (*i)[2]< 0.80) {(output)[output_ctr][0] = 0;
-                        (output)[output_ctr][1] = 0;
-                        (output)[output_ctr][2] = -1;}
-            if((*i)[2]>0.80) {(output)[output_ctr][0] = 0;
+            (output)[output_ctr][0] = -r2 * (*i)[0];
+            (output)[output_ctr][1] = -r2 * (*i)[1];
+            (output)[output_ctr][2] = r2 * (*i)[2];
+            if((*i)[2]<0.8 && (*i)[2]>0.78 && pow((*i)[0],2)+pow((*i)[1],2)<0.9) {(output)[output_ctr][0] = 0;
                         (output)[output_ctr][1] = 0;
                         (output)[output_ctr][2] = 1;}
+            if((*i)[2]>0.80) {(output)[output_ctr][0] = 0;
+                        (output)[output_ctr][1] = 0;
+                        (output)[output_ctr][2] = -1;}
+            if((*i)[2]>-0.8 && (*i)[2]<-0.78 && pow((*i)[0],2)+pow((*i)[1],2)<0.9) {(output)[output_ctr][0] = 0;
+                        (output)[output_ctr][1] = 0;
+                        (output)[output_ctr][2] = 1;}
+            if((*i)[2]<-0.80) {(output)[output_ctr][0] = 0;
+                        (output)[output_ctr][1] = 0;
+                        (output)[output_ctr][2] = -1;}
         }
     }
     bool integrity_invariant(){
