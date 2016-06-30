1594cd447a217ebba82c43718baff6662ba2ccfc
Michael Karotsieris <michael.karotsieris@gmail.com>
2016-06-30 16:32:11 +0100

    documented and cleaned up file

--- js_iteration_2/basic_data_structures.hpp
+++ js_iteration_2/basic_data_structures.hpp
@@ -13,19 +13,27 @@ typedef array1d::index  index_t;
 
 
 typedef array1d::index  vertex_t;
+
+/** Implementation Note
+ *  Data type: vectorized_scalar
+ *  -----------------------------
+ *  The type vectorized_scalar is a container for floats that has one dimension
+ *  so it can be thought of as a vector.This data structure aims to be a container
+ *  for the evaluated implicit function values. 
+ */
+
 typedef boost::multi_array<REAL, 1>  vectorized_scalar;
-//typedef boost::multi_array<REAL, 2>  vectorized_x;
-//typedef boost::multi_array<REAL, 2>  vectorized_vect3d;
-// vectorized_vect3d   vectorized_x  assert_3x
+
+/** Implementation Note
+ * Data type: vectorized_vect
+ * --------------------------
+ *
+ * The type vectorized_vect is a container for floats and has two dimensions.
+ * It aims to be a container for 3d coordinates, so a variable of this type
+ * will be a N x 3 array.
+ */
 typedef boost::multi_array<REAL, 2>  vectorized_vect;
 
-
-//vectorized_vect z = {{-1.,4,5}};
-
-/*
-typedef boost::multi_array<index3_t, 1>   array1d_e3;
-typedef std::map<index3_t,int>  e3map_t;
-*/
 
 #include ".//my_assert.hpp"
 
@@ -38,17 +46,3 @@ inline array_shape_t make_shape_1d(array1d::index size) {
 
 
 typedef boost::multi_array<REAL, 2>  array2d;
-
-/*
-inline bool assert_3x_output(const array2d& x, array1d& output){
-    assert(x.shape()[1] == output.shape()[0]);
-    assert(x.shape()[1] == output.shape()[0]);
-}
-*/
-/*
-inline bool assert_3x(const array2d& x){
-    int n = x.shape()[0];
-    assert(x.shape()[1] == output.shape()[0]);
-    assert(x.shape()[1] == output.shape()[0]);
-}
-*/
