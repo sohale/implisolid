8c6865cc17b10d090cc240414d48594ad39b0621
schauvier <s.chauvier@gmail.com>
2016-06-30 17:44:31 +0100

    Adding two more tests to compare python and cpp

--- clean_code/tta.py
+++ clean_code/tta.py
@@ -20,8 +20,10 @@ faces_p = np.zeros((4796, 3))
 centroids_p = np.zeros((4796, 3))
 norm_old_vertex_p = np.zeros(2400)
 norm_new_vertex_p = np.zeros(2400)
+norm_centroids_p = np.zeros(4796)
 num_neig_vertex_p = np.zeros(2400)
-
+edge_length_o_p = np.zeros(4796)
+edge_length_n_p = np.zeros(4796)
 # creating the arrays
 
 i = 0
@@ -89,6 +91,35 @@ for j in range(faces_p.shape[0]):
 mean_num_neig_v_p = np.mean(num_neig_vertex_p)
 
 # *************************
+# ********  test3  ********
+# *************************
+
+norm_centroids_p = np.linalg.norm(centroids_p, axis=1)
+
+mean_norm_centroids_p = np.mean(norm_centroids_p)
+
+d_m_ir_c_p = abs(mean_norm_centroids_p - 1.)
+
+
+# *************************
+# ********  test4  ********
+# *************************
+
+for j in range(faces_p.shape[0]):
+    edge_length_o_p[j] += np.linalg.norm((old_vertex_p[faces_p[j][0]][0] - old_vertex_p[faces_p[j][1]][0], old_vertex_p[faces_p[j][0]][1] - old_vertex_p[faces_p[j][1]][1], old_vertex_p[faces_p[j][0]][2] - old_vertex_p[faces_p[j][1]][2]))
+    edge_length_o_p[j] += np.linalg.norm((old_vertex_p[faces_p[j][0]][0] - old_vertex_p[faces_p[j][2]][0], old_vertex_p[faces_p[j][0]][1] - old_vertex_p[faces_p[j][2]][1], old_vertex_p[faces_p[j][0]][2] - old_vertex_p[faces_p[j][2]][2]))
+    edge_length_o_p[j] += np.linalg.norm((old_vertex_p[faces_p[j][2]][0] - old_vertex_p[faces_p[j][1]][0], old_vertex_p[faces_p[j][2]][1] - old_vertex_p[faces_p[j][1]][1], old_vertex_p[faces_p[j][2]][2] - old_vertex_p[faces_p[j][1]][2]))
+
+    edge_length_n_p[j] += np.linalg.norm((new_vertex_p[faces_p[j][0]][0] - new_vertex_p[faces_p[j][1]][0], new_vertex_p[faces_p[j][0]][1] - new_vertex_p[faces_p[j][1]][1], new_vertex_p[faces_p[j][0]][2] - new_vertex_p[faces_p[j][1]][2]))
+    edge_length_n_p[j] += np.linalg.norm((new_vertex_p[faces_p[j][0]][0] - new_vertex_p[faces_p[j][2]][0], new_vertex_p[faces_p[j][0]][1] - new_vertex_p[faces_p[j][2]][1], new_vertex_p[faces_p[j][0]][2] - new_vertex_p[faces_p[j][2]][2]))
+    edge_length_n_p[j] += np.linalg.norm((new_vertex_p[faces_p[j][2]][0] - new_vertex_p[faces_p[j][1]][0], new_vertex_p[faces_p[j][2]][1] - new_vertex_p[faces_p[j][1]][1], new_vertex_p[faces_p[j][2]][2] - new_vertex_p[faces_p[j][1]][2]))
+
+
+edge_mean_o_p = np.mean(edge_length_o_p)/3
+edge_mean_n_p = np.mean(edge_length_n_p)/3
+
+
+# *************************
 # *********  C++  *********
 # *************************
 
@@ -99,7 +130,9 @@ centroids_cpp = np.zeros((4808, 3))
 norm_old_vertex_cpp = np.zeros(2406)
 norm_new_vertex_cpp = np.zeros(2406)
 num_neig_vertex_cpp = np.zeros(2406)
-
+norm_centroids_cpp = np.zeros(4808)
+edge_length_o_cpp = np.zeros(4808)
+edge_length_n_cpp = np.zeros(4808)
 # creating the arrays
 
 i = 0
@@ -160,6 +193,36 @@ for j in range(faces_cpp.shape[0]):
 
 mean_num_neig_v_cpp = np.mean(num_neig_vertex_cpp)
 
+
+# *************************
+# ********  test3  ********
+# *************************
+
+norm_centroids_cpp = np.linalg.norm(centroids_cpp, axis=1)
+
+mean_norm_centroids_cpp = np.mean(norm_centroids_cpp)
+
+d_m_ir_c_cpp = abs(mean_norm_centroids_cpp - 0.8)/0.8
+
+
+# *************************
+# ********  test4  ********
+# *************************
+
+for j in range(faces_cpp.shape[0]):
+    edge_length_o_cpp[j] += np.linalg.norm((old_vertex_cpp[faces_cpp[j][0]][0] - old_vertex_cpp[faces_cpp[j][1]][0], old_vertex_cpp[faces_cpp[j][0]][1] - old_vertex_cpp[faces_cpp[j][1]][1], old_vertex_cpp[faces_cpp[j][0]][2] - old_vertex_cpp[faces_cpp[j][1]][2]))
+    edge_length_o_cpp[j] += np.linalg.norm((old_vertex_cpp[faces_cpp[j][0]][0] - old_vertex_cpp[faces_cpp[j][2]][0], old_vertex_cpp[faces_cpp[j][0]][1] - old_vertex_cpp[faces_cpp[j][2]][1], old_vertex_cpp[faces_cpp[j][0]][2] - old_vertex_cpp[faces_cpp[j][2]][2]))
+    edge_length_o_cpp[j] += np.linalg.norm((old_vertex_cpp[faces_cpp[j][2]][0] - old_vertex_cpp[faces_cpp[j][1]][0], old_vertex_cpp[faces_cpp[j][2]][1] - old_vertex_cpp[faces_cpp[j][1]][1], old_vertex_cpp[faces_cpp[j][2]][2] - old_vertex_cpp[faces_cpp[j][1]][2]))
+
+    edge_length_n_cpp[j] += np.linalg.norm((new_vertex_cpp[faces_cpp[j][0]][0] - new_vertex_cpp[faces_cpp[j][1]][0], new_vertex_cpp[faces_cpp[j][0]][1] - new_vertex_cpp[faces_cpp[j][1]][1], new_vertex_cpp[faces_cpp[j][0]][2] - new_vertex_cpp[faces_cpp[j][1]][2]))
+    edge_length_n_cpp[j] += np.linalg.norm((new_vertex_cpp[faces_cpp[j][0]][0] - new_vertex_cpp[faces_cpp[j][2]][0], new_vertex_cpp[faces_cpp[j][0]][1] - new_vertex_cpp[faces_cpp[j][2]][1], new_vertex_cpp[faces_cpp[j][0]][2] - new_vertex_cpp[faces_cpp[j][2]][2]))
+    edge_length_n_cpp[j] += np.linalg.norm((new_vertex_cpp[faces_cpp[j][2]][0] - new_vertex_cpp[faces_cpp[j][1]][0], new_vertex_cpp[faces_cpp[j][2]][1] - new_vertex_cpp[faces_cpp[j][1]][1], new_vertex_cpp[faces_cpp[j][2]][2] - new_vertex_cpp[faces_cpp[j][1]][2]))
+
+
+edge_mean_o_cpp = np.mean(edge_length_o_cpp)/(3*0.8)
+edge_mean_n_cpp = np.mean(edge_length_n_cpp)/(3*0.8)
+
+
 # writing into the file
 
 file_test.write("Mean distance between the computed points and the ideal point of the sphere for the old vertex in python: " + str(d_m_ir_o_p) + "\n")
@@ -170,7 +233,15 @@ file_test.write("Mean distance between the computed points and the ideal point o
 file_test.write("\n")
 file_test.write("Mean number of neighbours of vertex in python: " + str(mean_num_neig_v_p) + "\n")
 file_test.write("Mean number of neighbours of vertex in cpp:    " + str(mean_num_neig_v_cpp) + "\n")
-
+file_test.write("\n")
+file_test.write("Mean distance between the computed points and the ideal point of the sphere for the centroids in python: " + str(d_m_ir_c_p) + "\n")
+file_test.write("Mean distance between the computed points and the ideal point of the sphere for the centroids in cpp:    " + str(d_m_ir_c_cpp) + "\n")
+file_test.write("\n")
+file_test.write("Mean edge length for the old vertex in python: " + str(edge_mean_o_p) + "\n")
+file_test.write("Mean edge length for the old vertex in cpp:    " + str(edge_mean_o_cpp) + "\n")
+file_test.write("\n")
+file_test.write("Mean edge length for the new vertex in python: " + str(edge_mean_n_p) + "\n")
+file_test.write("Mean edge length for the new vertex in cpp:    " + str(edge_mean_n_cpp) + "\n")
 
 python_data.close()
 cpp_data.close()
