1b1702cb5320a12abba0effb16d3b0099c3d621f
sohale <sohale@gmail.com>
2016-06-30 14:44:43 +0100

    new em++ scripts,  folder name moved to em_prepare.sh

--- js_iteration_1/build_mcc2.sh
+++ js_iteration_1/build_mcc2.sh
@@ -2,14 +2,27 @@
 
 # echo "@rem em++ -I /usr/local/inlcude/boost_1_61_0/ -s EXPORTED_FUNCTIONS="['_main']" -pedantic -std=c++14  -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS  -O3     vect_test_2.cpp    -o vect_test_2.js"
 
-BOOST_FOLDER=/usr/local/include/boost_1_61_0
+
+#BOOST_FOLDER=/usr/local/include/boost_1_61_0
+#BOOST_FOLDER=./boost_1_61_0
+
+
+#if [ -z ${EM_PREPARE+x} ]
+if [[ -n "$EM_PREPARE" ]]
+then
+    echo ""
+else
+    echo "First run \"source em_prepare.sh\""
+    exit
+fi
+
 OPTIM=1
 if [ $OPTIM -eq 0 ]
 then
 
     echo "** dev version **"
 
-    ~/emsdk_portable/emscripten/master/em++ -I $BOOST_FOLDER  \
+    em++ -I $BOOST_FOLDER  \
         -s TOTAL_MEMORY=30100100 \
         -s EXPORTED_FUNCTIONS="['_produce_object_old2', '_main', '_build_geometry', '_get_v_size', '_get_f_size', '_get_f', '_get_v', '_finish_geometry', '_get_f_ptr', '_get_v_ptr']" \
         -s NO_EXIT_RUNTIME=1 \
---
+++ js_iteration_1/em_prepare.sh
@@ -0,0 +1,12 @@
+
+#export BOOST_FOLDER=/usr/local/include/boost_1_61_0
+
+export BOOST_FOLDER=./boost_1_61_0
+
+export EM_PREPARE=1
+
+#set PATH
+
+~/mp5/mp5-private/solidmodeler/js_iteration_1/install/emsdk_portable/emsdk activate latest
+
+source ~/mp5/mp5-private/solidmodeler/js_iteration_1/install/emsdk_portable/emsdk_env.sh
