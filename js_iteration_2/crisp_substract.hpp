efb47a122e46bd9d404fb3b3f2431acb79d36177
Michael Karotsieris <michael.karotsieris@gmail.com>
2016-07-04 12:36:00 +0100

    added crisp subtract header file

--- js_iteration_2/crisp_substract.hpp
+++ js_iteration_2/crisp_substract.hpp
@@ -1,3 +1,8 @@
+#pragma once
+
+#include "implicit_function.hpp"
+#include "basic_data_structures.hpp"
+
 /**
  * File: crisp_subtract.cpp
  * ------------------------
@@ -14,17 +19,45 @@ public:
 
     /**
      * Function Declarations:
-     * CrispSubtract --> The constructor of the class.
-     * eval_implicit --> evaluation of the implicit function of self.
-     * eval_gradient --> evaluation of the implicit gradient of self.
+     * CrispSubtract(a, b) --> The constructor of the class.
+     * 		a and b are of type implicit_function, and need to be evaluated
+     * 		on a grid or vector.
+     *
+     * eval_implicit --> evaluation of the implicit function.
+     * eval_gradient --> evaluation of the implicit gradient.
      * ~CrispSubtract --> Deconstructor of the class.
      */
-    // define the constructor
 
-    CrispSubtract(implicit_function a, implicit_function b);
-    void eval_implicit(const vectorized_vect& x, vectorized_scalar* output);
-    void eval_gradient(const vectorized_vect& x, vectorized_scalar* output);
-    ~CrispSubtract();
+    CrispSubtract(const implicit_function & a_, const implicit_function & b_)
+    : a(a_),b(b_)
+    {
+        // of what type is a and b fa
+    }
+    void eval_implicit(const vectorized_vect& x, vectorized_scalar* f_output){
+        int output_ctr = 0;
+        auto i = x.begin();
+        auto e = x.end();
+
+        const int nsize = x.shape()[0];
+
+        auto sf = make_shape_1d(nsize);
+
+        vectorized_scalar f1 = vectorized_scalar(sf);  // first function
+        vectorized_scalar f2 = vectorized_scalar(sf);  // second function
+
+        a.eval_implicit(x, &f1);
+        b.eval_implicit(x, &f2);
+
+        for (; i < e; i++, output_ctr++){
+            (*f_output)[output_ctr] = (f1[output_ctr] < -f2[output_ctr]) ? (f1[output_ctr]): -f2[output_ctr];
+        }
+    }
+    void eval_gradient(const vectorized_vect& x, vectorized_scalar* output){
+
+    }
+
+    ~CrispSubtract();   // will this compile with no warning? 
+
+private:
+    const implicit_function &a, &b;  // reference a and b, this should be considered again.
 };
-
-CrispSubtract::eval_implicit(const vectorized_vect& x, vectorized_scalar* output){};
