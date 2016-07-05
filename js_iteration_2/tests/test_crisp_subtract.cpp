162ea6473b26f1ff6363d7b4313f4ff051aa78cc
Michael Karotsieris <michael.karotsieris@gmail.com>
2016-07-05 17:21:58 +0100

    set up tests for crisp subtract

---
+++ js_iteration_2/tests/test_crisp_subtract.cpp
@@ -0,0 +1,46 @@
+/**
+* File: test_crisp_subtract.cpp
+*------------------------------
+*
+* In this file there are tests for the crisp subtraction operation.
+*/
+
+#include "../primitives.cpp"
+#include "../basic_data_structures.hpp"
+#include "../crisp_subtract.hpp"
+#include "../unit_sphere.hpp"
+#include "gtest/gtest.h"
+// #include "../timer.hpp"
+
+TEST(CrispSubtractTests, TwoSpheres) {
+
+    const int nsize = 3;    // size of input vector
+    auto shape_tuple = make_shape_1d(nsize);
+    vectorized_scalar f = vectorized_scalar(shape_tuple);
+
+    vectorized_vect x = make_empty_x(nsize);
+
+    // 1st point , the origin.
+    x[0][0] = 0.0;
+    x[0][1] = 0.0;
+    x[0][2] = 0.0;
+
+    mp5_implicit::unit_sphere s1(2.0);
+    mp5_implicit::unit_sphere s2(1.3);
+
+    CrispSubtract crs = CrispSubtract(s1,s2);
+    crs.eval_implicit(x, &f);
+    // see the output for the first point
+    std::cout << f[0] << std::endl;
+    }
+
+void linspace(REAL x_start, REAL x_end, int num_steps){
+
+}
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    std::cout << "Good bye." << std::endl;
+    return RUN_ALL_TESTS();
+}
