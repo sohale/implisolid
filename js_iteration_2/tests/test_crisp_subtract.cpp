e8198cefe02dd87133b252ac8d727cf3aab98569
Michael Karotsieris <michael.karotsieris@gmail.com>
2016-07-06 16:25:03 +0100

    added test for crisp_subtract

--- js_iteration_2/tests/test_crisp_subtract.cpp
+++ js_iteration_2/tests/test_crisp_subtract.cpp
@@ -14,30 +14,43 @@
 // #include "../timer.hpp"
 
 TEST(CrispSubtractTests, TwoSpheres) {
-
+/**
+ * Description:
+ * Creates 2 spheres, takes the subtraction and tests the sign of the implicit function
+ * correctness on 3 points.
+ */
     const int nsize = 3;    // size of input vector
     auto shape_tuple = make_shape_1d(nsize);
     vectorized_scalar f = vectorized_scalar(shape_tuple);
 
     vectorized_vect x = make_empty_x(nsize);
 
-    // 1st point, the origin.
-    x[0][0] = 0.0;
-    x[0][1] = 0.0;
-    x[0][2] = 0.0;
-
-    // // 2nd point,
-    // x[0][0] = 0.0;
-    // x[0][1] = 0.0;
-    // x[0][2] = 0.0;
-
     mp5_implicit::unit_sphere s1(2.0);
     mp5_implicit::unit_sphere s2(1.3);
 
     CrispSubtract crs = CrispSubtract(s1,s2);
+
+    // 1st point
+    x[0][0] = 0.0;
+    x[0][1] = 0.0;
+    x[0][2] = 0.0;
+
+    // 2nd point
+    x[1][0] = 1.0;
+    x[1][1] = 1.0;
+    x[1][2] = 2.0;
+
+    // 3rd point
+    x[2][0] = 1.0;
+    x[2][1] = 1.0;
+    x[2][2] = 1.0;
+
+
     crs.eval_implicit(x, &f);
     // see the output for the first point
     EXPECT_LT( f[0], -ROOT_TOLERANCE );
+    EXPECT_LT( f[1], -ROOT_TOLERANCE );
+    EXPECT_GT( f[2], +ROOT_TOLERANCE );
 }
 
 void linspace(REAL x_start, REAL x_end, int num_steps){
