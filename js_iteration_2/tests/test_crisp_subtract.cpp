ea19a2f651fdec31847e942326ca87f458a01a5f
Michael Karotsieris <michael.karotsieris@gmail.com>
2016-07-12 11:40:57 +0100

    added test case for two overlapping spheres

--- js_iteration_2/tests/test_crisp_subtract.cpp
+++ js_iteration_2/tests/test_crisp_subtract.cpp
@@ -13,11 +13,11 @@
 
 // #include "../timer.hpp"
 
-TEST(CrispSubtractTests, TwoSpheres) {
+TEST(CrispSubtractTests, TwoSpheresOneInsideOther) {
 /**
  * Description:
- * Creates 2 spheres, takes the subtraction and tests the sign of the implicit function
- * correctness on 3 points.
+ * Creates 2 spheres, takes the subtraction and tests the sign of the implicit
+ * function correctness on 3 points.
  */
     const int nsize = 3;    // size of input vector
     auto shape_tuple = make_shape_1d(nsize);
@@ -28,19 +28,20 @@ TEST(CrispSubtractTests, TwoSpheres) {
     mp5_implicit::unit_sphere s1(2.0);
     mp5_implicit::unit_sphere s2(1.3);
 
-    CrispSubtract crs = CrispSubtract(s1,s2);
+    mp5_implicit::CrispSubtract crs = mp5_implicit::CrispSubtract(s1,s2);
 
-    // 1st point x1**2 + y1**2 + z1**2 = 0, inside both spheres, should output -
+    // 1st point x1**2 + y1**2 + z1**2 = 0,inside both spheres, should output -
     x[0][0] = 0.0;
     x[0][1] = 0.0;
     x[0][2] = 0.0;
 
-    // 2nd point x2**2 + y2**2 + z2**2 = 6 , outside both spheres, should output -
+    // 2nd point x2**2 + y2**2 + z2**2 = 6,outside both spheres,should output -
     x[1][0] = 1.0;
     x[1][1] = 1.0;
     x[1][2] = 2.0;
 
-    // 3rd point , x3**2 + y3**2 + z3**2 = 3 , inside big, outside of small, should output +
+    // 3rd point , x3**2 + y3**2 + z3**2 = 3 , inside big, outside of small,
+    // should output +
     x[2][0] = 1.0;
     x[2][1] = 1.0;
     x[2][2] = 1.0;
@@ -52,6 +53,50 @@ TEST(CrispSubtractTests, TwoSpheres) {
     EXPECT_LT( f[1], -ROOT_TOLERANCE );
     EXPECT_GT( f[2], +ROOT_TOLERANCE );
 }
+
+TEST(CrispSubtractTests, TwoSpheresOverlapping){
+    /**
+     * Description:
+     * Creates 2 spheres, takes the subtraction and tests the sign of the
+     * implicit function correctnes on 3 points. The difference with the above
+     * test is that the two spheres now overlap instead of one being inside the
+     * other.
+     */
+
+     const int nsize = 3;    // size of input vector
+     auto shape_tuple = make_shape_1d(nsize);
+     vectorized_scalar f = vectorized_scalar(shape_tuple);
+
+     vectorized_vect x = make_empty_x(nsize);
+
+     mp5_implicit::unit_sphere s1(1.5, -1, 0, 0);
+     mp5_implicit::unit_sphere s2(1.5, 1, 0, 0);
+
+     mp5_implicit::CrispSubtract crs = mp5_implicit::CrispSubtract(s1,s2);
+
+     // 1st point x1**2 + y1**2 + z1**2 = 0,outside both spheres, should output -
+     x[0][0] = -3.0;
+     x[0][1] = 0.0;
+     x[0][2] = 0.0;
+
+     // 2nd point should output +
+     x[1][0] = -1.75;
+     x[1][1] = 0.2;
+     x[1][2] = 0.2;
+
+     // 3rd point should output -
+     x[2][0] = 0.0;
+     x[2][1] = 0.0;
+     x[2][2] = 0.0;
+
+
+     crs.eval_implicit(x, &f);
+     // see the output for the first point
+     EXPECT_LT( f[0], -ROOT_TOLERANCE );
+     EXPECT_GT( f[1], +ROOT_TOLERANCE );
+     EXPECT_LT( f[2], -ROOT_TOLERANCE );
+}
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
