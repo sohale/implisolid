8d1a29ab091c19d1300e8e2dbb4f7708608944e5
schauvier <s.chauvier@gmail.com>
2016-07-12 16:03:35 +0100

    Rotation for the super cone aka the scone

--- js_iteration_2/scone.hpp
+++ js_iteration_2/scone.hpp
@@ -2,12 +2,14 @@
 #include "basic_data_structures.hpp"
 namespace mp5_implicit {
 
-class scone : public implicit_function {
+class scone : public transformable_implicit_function {
 
 protected:
     REAL r;
     REAL a; REAL b; REAL c;
     REAL x; REAL y; REAL z;
+    REAL* transf_matrix;
+    REAL* inv_transf_matrix;
 
 public:
     scone(REAL height, REAL radius_x, REAL radius_y, REAL radius_increase_speed ){
@@ -18,6 +20,19 @@ public:
         this->x = 0.;
         this->y = 0.;
         this->z = 0.;
+
+        this->transf_matrix = new REAL [12];
+        this->inv_transf_matrix = new REAL [12];
+        for (int i=0; i<12; i++){
+          if(i==0 || i==5 || i==10){
+            this->transf_matrix[i] = 1;
+            this->inv_transf_matrix[i] = 1;
+          }
+          else{
+            this->transf_matrix[i] = 0;
+            this->inv_transf_matrix[i] = 0;
+          }
+        }
     }
 
     scone(REAL height, REAL radius_x, REAL radius_y, REAL radius_increase_speed, REAL center_x, REAL center_y, REAL center_z ){
@@ -28,11 +43,73 @@ public:
         this->x = center_x;
         this->y = center_y;
         this->z = center_z;
+
+        this->transf_matrix = new REAL [12];
+        this->inv_transf_matrix = new REAL [12];
+        for (int i=0; i<12; i++){
+          if(i==0 || i==5 || i==10){
+            this->transf_matrix[i] = 1;
+            this->inv_transf_matrix[i] = 1;
+          }
+          else{
+            this->transf_matrix[i] = 0;
+            this->inv_transf_matrix[i] = 0;
+          }
+        }
+    }
+
+    virtual void rotate(const REAL angle, const vectorized_vect axis) const {
+      REAL ca = cos(angle);
+      REAL sa = sin(angle);
+      REAL norm = sqrt(axis[0][0]*axis[0][0] + axis[0][1]*axis[0][1] + axis[0][2]*axis[0][2]);
+      REAL a1 = axis[0][0]/norm;
+      REAL a2 = axis[0][1]/norm;
+      REAL a3 = axis[0][2]/norm;
+
+      REAL rotation[12];
+      rotation[0] = ca + a1*a1*(1.-ca);
+      rotation[1] = a1*a2*(1.-ca) - a3*sa;
+      rotation[2] = a1*a3*(1.-ca) + a2*sa;
+      rotation[3] = 0.;
+      rotation[4] = a1*a2*(1.-ca) + a3*sa;
+      rotation[5] = ca + a2*a2*(1.-ca);
+      rotation[6] = a2*a3*(1.-ca) - a1*sa;
+      rotation[7] = 0.;
+      rotation[8] = a1*a3*(1.-ca) - a2*sa;
+      rotation[9] = a2*a3*(1.-ca) + a1*sa;
+      rotation[10] = ca + a3*a3*(1.-ca);
+      rotation[11] = 0.;
+
+      Matrix_Matrix_Product(this->transf_matrix, rotation);
+
+      InvertMatrix(this->transf_matrix, this->inv_transf_matrix);
+
+    }
+
+    virtual void move(const vectorized_vect direction) const{
+      this->transf_matrix[3] += direction[0][0];
+      this->transf_matrix[7] += direction[0][1];
+      this->transf_matrix[11] += direction[0][2];
+      InvertMatrix(this->transf_matrix, this->inv_transf_matrix);
+
+    }
+    virtual void resize(const REAL ratio) const{
+      for (int i=0; i<12; i++){
+        if(i==3 || i==7 || i==11){
+        }
+        else{
+        this->transf_matrix[i] *= ratio;
+        }
+      }
+      InvertMatrix(this->transf_matrix, this->inv_transf_matrix);
     }
 
     virtual void eval_implicit(const vectorized_vect& x, vectorized_scalar* f_output) const {
         my_assert(assert_implicit_function_io(x, *f_output), "");
         my_assert(this->integrity_invariant(), "");
+        vectorized_vect x_copy = x;
+
+        Matrix_Vector_Product(this->inv_transf_matrix, x_copy);
 
         const REAL a2 = squared(this->a);
         const REAL b2 = squared(this->b);
@@ -40,8 +117,8 @@ public:
 
         int output_ctr=0;
 
-        auto i = x.begin();
-        auto e = x.end();
+        auto i = x_copy.begin();
+        auto e = x_copy.end();
         for(; i<e; i++, output_ctr++){
           if((*i)[2]-this->z >= 0.0){
             (*f_output)[output_ctr] = -((*i)[0]-this->x)*((*i)[0]-this->x)/a2 - ((*i)[1]-this->y)*((*i)[1]-this->y)/b2 + ((*i)[2]-this->z)*((*i)[2]-this->z)/c2;
@@ -54,12 +131,17 @@ public:
         }
     }
     virtual void eval_gradient(const vectorized_vect& x, vectorized_vect* output) const {
+
+        vectorized_vect x_copy = x;
+
+        Matrix_Vector_Product(this->inv_transf_matrix, x_copy);
+
         const REAL a2 = squared(this->a);
         const REAL b2 = squared(this->b);
         const REAL c2 = squared(this->c);
         int output_ctr=0;
-        auto i = x.begin();
-        auto e = x.end();
+        auto i = x_copy.begin();
+        auto e = x_copy.end();
         const REAL r2 = squared(this->r);
         for(; i!=e; i++, output_ctr++){
 
