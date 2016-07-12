3d9fc4a77ee97bd497f504653e91fd33682fee3a
schauvier <s.chauvier@gmail.com>
2016-07-12 16:07:02 +0100

    Rotation work for the scylinder

--- js_iteration_1/mcc2_MS.cpp
+++ js_iteration_1/mcc2_MS.cpp
@@ -229,7 +229,7 @@ void build_geometry(int resolution, REAL mc_size, REAL time){
       direction[0][1] = 0.3;
       direction[0][2] = 0.3;
 
-      scone segg(0.6, 0.3, 0.3, 2);
+      scylinder segg(0.4, 1.3);
       // segg.move(direction);
       // segg.resize(12.);
       segg.rotate(2., direction);
