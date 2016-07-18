e332becaf5c503cbce144cd01cc640ddd05bf76f
Michael Karotsieris <michael.karotsieris@gmail.com>
2016-07-18 19:00:24 +0100

    modified build script to have an option for outputting LLVM bitcode

--- js_iteration_1/build_mcc2.sh
+++ js_iteration_1/build_mcc2.sh
@@ -8,6 +8,7 @@
 
 
 #if [ -z ${EM_PREPARE+x} ]
+
 if [[ -n "$EM_PREPARE" ]]
 then
     echo ""
@@ -16,37 +17,65 @@ else
     exit
 fi
 
-case "$1" in
-    0)
-    OPTIM=0
-    ;;
-    1)
-    OPTIM=1
-    ;;
-    *)
-    echo "Select how to compile [0 / 1] or press Enter for default(Development)"
-    echo
+OPTIM=0
+SAVE_BC=0
 
-    echo Development Version: 0
-    echo Optimized Version: 1
-    read OPTIM
-    ;;
-esac
 
-case "$OPTIM" in
-    0)
-    echo "Will now compile in Development Version, optimizations are turned off"
-    OPTIM=0
-    ;;
-    1)
-    echo "Will now compile in Optimized Version, optimizations are turned on"
-    OPTIM=1
-    ;;
-    *)
-    OPTIM=0
-    echo "Running default, Development Version"
-    ;;
-esac
+TEMP=`getopt -o dob -l dev,opt,bitcode -- "$@"`
+eval set -- "$TEMP"
+# echo $TEMP
+# extract options and their arguments into variables 
+USAGE=$'Usage: ./build_mcc2.sh [option]
+        -d or --dev for development version
+        -o or --opt for optimized version 
+        -b or --bitcode to emit llvm Code'
+
+if [ "$#" -lt 2 ]; then 
+    echo "$USAGE"
+    exit 1 
+fi 
+
+while true; do
+    case "$1" in 
+        -d|--dev)
+            OPTIM=0
+            shift
+            ;;
+        -o|--opt)
+            OPTIM=1
+            shift
+            ;;
+        -b|--bitcode)
+            SAVE_BC=1          
+            shift 
+            ;;
+        --)                                                                 
+            shift; break ;;            
+        *) echo "Internal Error"; exit 1 ;;
+    esac
+done
+
+if [ $SAVE_BC -eq 1 ]
+then
+
+    echo "Optimized version, bytecode will be emitted in the file " mcc2.ll
+    em++ \
+            --save-bc mcc2.bc \
+            -I $BOOST_FOLDER   \
+            -O3   \
+            --profiling     \
+            -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS  \
+            -s EXPORTED_FUNCTIONS="['_produce_object_old2', '_main', '_build_geometry', '_get_v_size', '_get_f_size', '_get_f', '_get_v', '_finish_geometry', '_get_f_ptr', '_get_v_ptr',   '_set_object', '_unset_object', '_set_x', '_unset_x', '_calculate_implicit_values', '_get_values_ptr', '_get_values_size', '_calculate_implicit_gradients', '_get_gradients_ptr', '_get_gradients_size' ]" \
+            -s NO_EXIT_RUNTIME=1          \
+            -Winline         \
+            -s TOTAL_MEMORY=30100100    \
+            -pedantic -std=c++14  \
+        mcc2.cpp  \
+            -o  mcc2.compiled.js
+    echo "Producing human readable LLVM ... "
+    llvm-dis mcc2.bc -o mcc2.ll && rm mcc2.bc # transform to human-readable form and delete bytecode file.
+    exit 0
+fi
 
 if [ $OPTIM -eq 0 ]
 then
@@ -60,9 +89,9 @@ then
         -s DEMANGLE_SUPPORT=1 \
          -s ASSERTIONS=1 \
         -pedantic -std=c++14 \
-        --profiling \
         mcc2.cpp -o mcc2.compiled.js
-
+#        --profiling \
+    exit 0 
 fi
 
 
@@ -82,36 +111,6 @@ then
             -pedantic -std=c++14  \
         mcc2.cpp  \
             -o  mcc2.compiled.js
+    exit 0
 fi
 
-# copy the path for em++
-# echo "@rem em++ -I /usr/local/inlcude/boost_1_61_0/ -s EXPORTED_FUNCTIONS="['_main']" -pedantic -std=c++14  -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS  -O3     vect_test_2.cpp    -o vect_test_2.js"
-#   ~/emsdk_portable/emscripten/master/em++ -I /usr/local/include/boost_1_61_0/  \
-#   -s TOTAL_MEMORY=30100100 -s EXPORTED_FUNCTIONS="['_produce_object_old2', '_main', '_build_geometry', '_get_v_size', '_get_f_size', '_get_f', '_get_v', '_finish_geometry', '_get_f_ptr', '_get_v_ptr']" -O3 -s NO_EXIT_RUNTIME=1 -s ASSERTIONS=1 -s DEMANGLE_SUPPORT=1 -pedantic -std=c++14 mcc2.cpp -o mcc2.cpp.js
-
-# ::     -s EXPORTED_FUNCTIONS="['_main', '_make_object']" ^
-# ::     -s EXPORTED_FUNCTIONS="['_main']" ^1
-# :: @rem     -O3   ^
-# :: @rem     -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS  ^
-#
-#
-# ::     -O3   ^
-# ::     -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS  ^
-#
-#
-# ::        -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS  ^
-# ::        -O3                           ^
-#
-
-# em++ ^
-#         -I C:\sohail\March\emscripten\boost_1_61_0\   ^
-#         -s ASSERTIONS=1               ^
-#         -s DEMANGLE_SUPPORT=1   ^
-#         -s TOTAL_MEMORY=30100100    ^
-#                                     ^
-#         -s EXPORTED_FUNCTIONS="['_produce_object_old2', '_main', '_build_geometry', '_get_v_size', '_get_f_size', '_get_f', '_get_v', '_finish_geometry', '_get_f_ptr', '_get_v_ptr']" ^
-#         -s NO_EXIT_RUNTIME=1          ^
-#                                           ^
-#         -pedantic -std=c++14  ^
-#     mcc2.cpp  ^
-#         -o  mcc2.cpp.js
