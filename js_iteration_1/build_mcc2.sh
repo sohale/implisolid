22f6fced16c518d4aa1a40d2df7d67de5f72ed3d
Michael Karotsieris <michael.karotsieris@gmail.com>
2016-07-18 11:23:23 +0100

    add profiling option when compiling to js

--- js_iteration_1/build_mcc2.sh
+++ js_iteration_1/build_mcc2.sh
@@ -73,6 +73,7 @@ then
     em++ \
             -I $BOOST_FOLDER   \
             -O3   \
+            --profiling     \
             -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS  \
             -s EXPORTED_FUNCTIONS="['_produce_object_old2', '_main', '_build_geometry', '_get_v_size', '_get_f_size', '_get_f', '_get_v', '_finish_geometry', '_get_f_ptr', '_get_v_ptr',   '_set_object', '_unset_object', '_set_x', '_unset_x', '_calculate_implicit_values', '_get_values_ptr', '_get_values_size', '_calculate_implicit_gradients', '_get_gradients_ptr', '_get_gradients_size' ]" \
             -s NO_EXIT_RUNTIME=1          \
