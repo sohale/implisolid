8bf31451743201d18ce1508fd50fa9cf62e2e4c7
charles <charles.cartiaux@gmail.com>
2016-08-01 12:14:37 +0100

    add torus in the designer

--- js_iteration_2/object_factory.hpp
+++ js_iteration_2/object_factory.hpp
@@ -192,6 +192,16 @@ implicit_function*  object_factory(pt::ptree shapeparams_dict, bool& use_metabal
         object = new mp5_implicit::heart(matrix12);
         register_new_object(object);
 
+    }else if(name == "itorus" ){
+        REAL matrix12[12];
+        getMatrix12(matrix12,shapeparams_dict);
+        if(ignore_root_matrix) {
+            copy_eye(matrix12);
+        }
+
+        object = new mp5_implicit::torus(matrix12);
+        register_new_object(object);
+
     }else if (name == "Union") {
         //todo: Use SimpleUnion if (matrix12 == eye(4))
         REAL matrix12[12];
