f55d5c6afc715ac01fca62e81886ea9763771307
Vitaliy Vrublevskiy <vitaliyvrublevskiy@gmail.com>
2016-10-19 23:00:22 +0300

    add min_z implicit and min_z gradient functions, everything is working

--- solidmodeler/optimization/main.cpp
+++ solidmodeler/optimization/main.cpp
@@ -8,17 +8,11 @@ using namespace dlib;
     Optimization tests for egg
 */
 
-const double a = 1., b = 1., c = 1.; // radius_x, radius_y, radius_z
+const double a = 1., b = 2., c = 3.; // radius_x, radius_y, radius_z
 const double center_x = 0., center_y = 0., center_z = 0.; // center of egg
-/*
-a = ;
-b = 1.;
-c = 1.;
 
-x0 = 0.;
-y0 = 0.;
-z0 = 0.;
-*/
+const double huge_number = 10000000.;
+
 typedef matrix<double,0,1> column_vector;
 
 inline double norm_squared(double x, double y, double z) {
@@ -27,6 +21,13 @@ inline double norm_squared(double x, double y, double z) {
 
 inline double squared(double x) {
     return x*x;
+}
+
+inline double my_abs(double x) {
+    if (x < 0) {
+        return -x;
+    }
+    return x;
 }
 
 double egg_eval_implicit (const column_vector& m)
@@ -59,19 +60,59 @@ const column_vector egg_eval_gradient  (const column_vector& m)
     return res;
 }
 
+double min_z_egg_eval_implicit (const column_vector& m)
+{
+    const double x = m(0); 
+    const double y = m(1);
+    const double z = m(2);
+
+    double res = egg_eval_implicit(m);
+    res = my_abs(res);
+
+    double res_z = z + huge_number * res;
+       
+    return res_z;
+}
+
+const column_vector min_z_egg_eval_gradient  (const column_vector& m)
+{
+    const double x = m(0);
+    const double y = m(1);
+    const double z = m(2);
+
+    column_vector res_grad_z(3);
+
+    double res = egg_eval_implicit(m); 
+    double sign = 1.;
+    
+    if (res < 0) {
+        sign = -1.;
+    }
+
+    const column_vector res_grad = egg_eval_gradient(m);
+
+    res_grad_z(0) = sign * huge_number * res_grad(0);
+    res_grad_z(1) = sign * huge_number * res_grad(1);
+    res_grad_z(2) = 1 + sign * huge_number * res_grad(2);
+
+    return res_grad_z;
+}
+
+
 int main()
 {
     try
     {
 
         column_vector starting_point(3);
+        /*
         starting_point = 1, 0, 0;
 
         cout << egg_eval_implicit(starting_point) << endl;
         cout << egg_eval_gradient(starting_point) << endl;
-
+*/
         starting_point = 0, 0, 0;
-
+/*
         cout << egg_eval_implicit(starting_point) << endl;
         cout << egg_eval_gradient(starting_point) << endl;
 
@@ -80,9 +121,19 @@ int main()
 
         cout << egg_eval_implicit(starting_point) << endl;
         cout << egg_eval_gradient(starting_point) << endl;
+*/
+        find_min_using_approximate_derivatives(bfgs_search_strategy(),
+                                               objective_delta_stop_strategy(1e-7),
+                                               min_z_egg_eval_implicit, starting_point, -1);
 
+        cout << starting_point << endl;
 
+        starting_point = 0, 0, 0;
+        find_min(bfgs_search_strategy(),
+                 objective_delta_stop_strategy(1e-7),
+                 min_z_egg_eval_implicit, min_z_egg_eval_gradient, starting_point, -1);
 
+        cout << starting_point << endl;
     }
     catch (std::exception& e)
     {
