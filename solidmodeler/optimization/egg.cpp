cd30e0e4d7fd803c15113f25c2d5e771de2f329e
Vitaliy Vrublevskiy <vitaliyvrublevskiy@gmail.com>
2016-10-21 23:37:11 +0300

    make classes, not functions

--- solidmodeler/optimization/egg.cpp
+++ solidmodeler/optimization/egg.cpp
@@ -6,9 +6,6 @@ using namespace dlib;
     Egg
 */
 
-const double a = 1., b = 2., c = 3.; // radius_x, radius_y, radius_z
-const double center_x = 0., center_y = 0., center_z = 0.; // center of egg
-
 typedef matrix<double,0,1> column_vector;
 
 inline double norm_squared(double x, double y, double z) {
@@ -19,32 +16,65 @@ inline double squared(double x) {
     return x*x;
 }
 
-double egg_eval_implicit (const column_vector& m)
-{
-    const double x = m(0); 
-    const double y = m(1);
-    const double z = m(2);
 
-    double egg_i = 1 - norm_squared((x - center_x)/a, (y - center_y)/b, (z - center_z)/c);
-       
-    return egg_i;
-}
+class Egg_Abstract {
+public:
+    double a, b, c; // radius_x, radius_y, radius_z
+    double center_x, center_y, center_z; // center of egg
 
-const column_vector egg_eval_gradient  (const column_vector& m)
-{
-    const double x = m(0);
-    const double y = m(1);
-    const double z = m(2);
+    Egg_Abstract(const column_vector& p) {
+        a = p(0);
+        b = p(1);
+        c = p(2);
+        center_x = p(3);
+        center_y = p(4);
+        center_z = p(5);
+    }
+};
 
-    const double a2 = squared(a);
-    const double b2 = squared(b);
-    const double c2 = squared(c);
 
-    column_vector egg_g(3);
+class Egg_Implicit : public Egg_Abstract{
+public:
+    Egg_Implicit(const column_vector& p) 
+    : Egg_Abstract(p) {
+        
+    }
 
-    egg_g(0) = -2. * (x - center_x)/a2;
-    egg_g(1) = -2. * (y - center_y)/b2;
-    egg_g(2) = -2. * (z - center_z)/c2;
+    double operator() ( const column_vector& m) const
+    {
+        const double x = m(0); 
+        const double y = m(1);
+        const double z = m(2);
 
-    return egg_g;
-}
+        double egg_i = 1 - norm_squared((x - center_x)/a, (y - center_y)/b, (z - center_z)/c);
+           
+        return egg_i;       
+    }
+};
+
+class Egg_Gradient : public Egg_Abstract{
+public:
+    Egg_Gradient(const column_vector& p) 
+    : Egg_Abstract(p) {
+        
+    }
+
+    column_vector operator() ( const column_vector& m) const
+    {
+        const double x = m(0);
+        const double y = m(1);
+        const double z = m(2);
+
+        const double a2 = squared(a);
+        const double b2 = squared(b);
+        const double c2 = squared(c);
+
+        column_vector egg_g(3);
+
+        egg_g(0) = -2. * (x - center_x)/a2;
+        egg_g(1) = -2. * (y - center_y)/b2;
+        egg_g(2) = -2. * (z - center_z)/c2;
+
+        return egg_g;  
+    }
+};
--- solidmodeler/optimization/main.cpp
+++ solidmodeler/optimization/main.cpp
@@ -22,79 +22,96 @@ inline double my_abs(double x) {
     return x;
 }
 
-double min_z_egg_eval_implicit (const column_vector& m)
-{
-    const double x = m(0); 
-    const double y = m(1);
-    const double z = m(2);
-
-    double egg_i = egg_eval_implicit(m);
-    egg_i = my_abs(egg_i);
-
-    double min_z_egg_i = z + huge_number * egg_i;
-       
-    return min_z_egg_i;
-}
-
-const column_vector min_z_egg_eval_gradient  (const column_vector& m)
-{
-    const double x = m(0);
-    const double y = m(1);
-    const double z = m(2);
-
-    column_vector min_z_egg_g(3);
-
-    double egg_i = egg_eval_implicit(m); 
-    double sign = 1.;
-    
-    if (egg_i < 0) {
-        sign = -1.;
+class Egg_Implicit_Min_Z : public Egg_Implicit{
+public:
+    Egg_Implicit_Min_Z(const column_vector& p) 
+    : Egg_Implicit(p)
+    {
+        ;
     }
 
-    const column_vector egg_g = egg_eval_gradient(m);
+    double operator() ( const column_vector& m) const
+    {
+        const double x = m(0); 
+        const double y = m(1);
+        const double z = m(2);
 
-    min_z_egg_g(0) = sign * huge_number * egg_g(0);
-    min_z_egg_g(1) = sign * huge_number * egg_g(1);
-    min_z_egg_g(2) = 1 + sign * huge_number * egg_g(2);
+        double egg_i = Egg_Implicit::operator()(m);
+        egg_i = my_abs(egg_i);
 
-    return min_z_egg_g;
-}
+        double min_z_egg_i = z + huge_number * egg_i;
+           
+        return min_z_egg_i;      
+    }
+};
 
+class Egg_Gradient_Min_Z : public Egg_Gradient {
+public:
+    Egg_Gradient_Min_Z(const column_vector& p) 
+    : Egg_Gradient(p) {
+        
+    }
+
+    const column_vector operator() ( const column_vector& m) const
+    {
+        column_vector p(6);
+        p = a, b, c, center_x, center_y, center_z;
+
+        const double x = m(0);
+        const double y = m(1);
+        const double z = m(2);
+
+        column_vector min_z_egg_g(3);
+        Egg_Implicit i(p);
+        double egg_i = i(m); 
+        double sign = 1.;
+        
+        if (egg_i < 0) {
+            sign = -1.;
+        }
+
+        const column_vector egg_g = Egg_Gradient::operator()(m);
+
+        min_z_egg_g(0) = sign * huge_number * egg_g(0);
+        min_z_egg_g(1) = sign * huge_number * egg_g(1);
+        min_z_egg_g(2) = 1 + sign * huge_number * egg_g(2);
+
+        return min_z_egg_g;    
+    }
+};
 
 int main()
 {
     try
     {
+        column_vector p(6);
+        p = 1, 2, 3, 0, 0, 0;
 
+        Egg_Implicit_Min_Z egg_i(p);
+        Egg_Gradient_Min_Z egg_g(p);
+        
         column_vector starting_point(3);
         
         starting_point = 0, 0, 0;
 
         auto start = Time::now();
-        
         find_min_using_approximate_derivatives(bfgs_search_strategy(),
                                                objective_delta_stop_strategy(1e-7),
-                                               min_z_egg_eval_implicit, starting_point, -1);
+                                               egg_i, starting_point, -1);
 
         auto end = Time::now();
         fsec duration = end - start;
-
         std::cout << "Duration : " << duration.count() << endl;
-
         cout << starting_point << endl;
 
         starting_point = 0, 0, 0;
 
         start = Time::now();
-
         find_min(bfgs_search_strategy(),
                  objective_delta_stop_strategy(1e-7),
-                 min_z_egg_eval_implicit, min_z_egg_eval_gradient, starting_point, -1);
-
-
+                 egg_i, egg_g, starting_point, -1);
         end = Time::now();
         duration = end - start;
-
         std::cout << "Duration : " << duration.count() << endl;
 
         cout << starting_point << endl;
