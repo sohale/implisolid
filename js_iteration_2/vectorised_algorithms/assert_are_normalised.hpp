20a0e1d37918288d5cd7360ecf50a2369d1927b5
Vitaliy Vrublevskiy <vitaliyvrublevskiy@gmail.com>
2016-11-28 21:50:20 +0200

    fix assert_are_normalised, add if for zero vector

--- js_iteration_2/vectorised_algorithms/assert_are_normalised.hpp
+++ js_iteration_2/vectorised_algorithms/assert_are_normalised.hpp
@@ -6,13 +6,23 @@ namespace mp5_implicit {
 namespace vectorised_algorithms {
 
 
+inline bool vector_are_zero(const REAL& x, const REAL& y, const REAL& z) {
+
+    return (
+        std::abs(x) < mp5_implicit::vectorised_algorithms::ALL_CLOSE_EPS &&
+        std::abs(y) < mp5_implicit::vectorised_algorithms::ALL_CLOSE_EPS &&
+        std::abs(z) < mp5_implicit::vectorised_algorithms::ALL_CLOSE_EPS
+    );
+}
+
 inline bool assert_are_normalised(const vectorized_vect& A) {
 
     for (vindex_t i = 0, e = A.shape()[0]; i < e; i++) {
         REAL norm2 = norm_squared(A[i][0], A[i][1], A[i][2]);
-        if(std::abs(norm2 - 1.0) < mp5_implicit::vectorised_algorithms::ALL_CLOSE_EPS)
-            {}
-        else {
+        if (vector_are_zero(A[i][0], A[i][1], A[i][2]) || 
+            std::abs(norm2 - 1.0) < mp5_implicit::vectorised_algorithms::ALL_CLOSE_EPS) {
+
+        } else {
             clog << " assersion failed at i=" << i << " " << A[i][0] << "," << A[i][1] << "," << A[i][2] << std::endl;
             return false;
         }
