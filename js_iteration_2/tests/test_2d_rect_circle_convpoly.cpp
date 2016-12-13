34257c22b1017399f123490f6b0733154c106af2
sohale <sohale@gmail.com>
2016-12-13 14:44:45 +0000

    better proper test for triangle (convex polygon). some nice refactoring: vector<> should[]

--- js_iteration_2/tests/test_2d_rect_circle_convpoly.cpp
+++ js_iteration_2/tests/test_2d_rect_circle_convpoly.cpp
@@ -11,10 +11,10 @@ using mp5_implicit::make_empty_x_2d;
 TEST(CircleTests, CenterPoint) {
 /**
  */
-    const int number_of_points = 3;    // size of input vector
-    auto shape_tuple = make_shape_1d(number_of_points);
+    const int number_of_test_points = 3;    // size of input vector
+    auto shape_tuple = make_shape_1d(number_of_test_points);
     vectorized_scalar f = vectorized_scalar(shape_tuple);
-    vectorized_vect x = make_empty_x_2d(number_of_points);
+    vectorized_vect x = make_empty_x_2d(number_of_test_points);
 
     // mp5_implicit::twodim::circle s1(2.0);
     mp5_implicit::circle cc(0,0, 1.3);
@@ -42,10 +42,12 @@ TEST(CircleTests, CenterPoint) {
 TEST(ConvexPolygonTests, Triangle) {
 /**
  */
-    const int number_of_points = 4;    // size of input vector
-    auto shape_tuple = make_shape_1d(number_of_points);
+    const int number_of_test_points = 6;    // size of input vector
+    auto shape_tuple = make_shape_1d(number_of_test_points);
     vectorized_scalar f = vectorized_scalar(shape_tuple);
-    vectorized_vect x = make_empty_x_2d(number_of_points);
+    vectorized_vect x = make_empty_x_2d(number_of_test_points);
+
+    std::vector<bool> should(number_of_test_points);
 
     int corners = 3;
     std::vector<REAL> xarray {0, 1, 2};
@@ -58,28 +60,46 @@ TEST(ConvexPolygonTests, Triangle) {
     mp5_implicit::concave_polygon cc (xarray, yarray);
 
     cout << "x:" << std::flush << std::endl;
-    x[0][0] = 0.0;
-    x[0][1] = 0.0;
+    x[0][0] = 0.0 + 0.1;
+    x[0][1] = 0.0 + 0.1/2;
+    should[0] = true;
 
     x[1][0] = 1.0;
-    x[1][1] = 1.0;
+    x[1][1] = 1.0 - 0.1;
+    should[1] = true;
 
-    x[2][0] = -1.0;
-    x[2][1] = -1.0;
+    x[2][0] = 1.0;
+    x[2][1] = 0.0 + 0.1;
+    should[2] = true;
 
-    x[3][0] = +1.0;
-    x[3][1] = -1.0;
+
+    x[3][0] = 2.0 - 0.1;
+    x[3][1] = 0.0 + 0.1/2;
+    should[3] = true;
+
+
+    x[4][0] = 1.0;
+    x[4][1] = -1.0;
+    should[4] = false;
+
+    x[5][0] = 1.0;
+    x[5][1] = 2.0;
+    should[5] = false;
 
     cout << "going to evaluate:" << std::flush << std::endl;
 
     cc.eval_implicit(x, &f);
-    cout << "f0:" << f[0];
-    cout << "f1:" << f[1];
-    cout << "f2:" << f[2] << std::endl;
 
-    EXPECT_GT( f[0], +ROOT_TOLERANCE );
-    EXPECT_LT( f[1], -ROOT_TOLERANCE );
-    EXPECT_LT( f[2], -ROOT_TOLERANCE );
+    for (int i = 0; i < should.size(); ++i) {
+        cout << "f["<< i<<"]:" << f[0] << "(" << (should[i]?"inner":"outside") <<") ";
+
+        if (should[i]) {
+            EXPECT_GT( f[i], +ROOT_TOLERANCE );
+        } else {
+            EXPECT_LT( f[i], -ROOT_TOLERANCE );
+        }
+    }
+    cout << std::endl <<  std::flush;
 }
 
 
