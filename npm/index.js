b35b7f053adca2fc355cd4cbefbd4fa423c33cf3
sohale <sohale@gmail.com>
2016-12-22 16:11:17 +0000

    npm'sindex.js updated

--- npm/index.js
+++ npm/index.js
@@ -1,6 +1,10 @@
 'use strict';
-console.log("Not ready. Please wait while we finalise the npm package.");
-loadScript('./js_iteration_2/js/worker_api.js');
-//importScripts('../../build/mcc2.compiled.js');
-loadScript('./js_iteration_2/js/implisolid_main.js');
-console.log("ImpliSolid");
+module.exports = function() {
+    console.log("Not ready. Please wait while we finalise the npm package.");
+    loadScript('./js_iteration_2/js/worker_api.js');
+    //importScripts('../../build/mcc2.compiled.js');
+    loadScript('./js_iteration_2/js/implisolid_main.js');
+    console.log("ImpliSolid");
+    return IMPLICIT;
+};
+
