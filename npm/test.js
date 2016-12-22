c97ed4d51d71eb8905043b5bf1bfebf66e8fa8e7
sohale <sohale@gmail.com>
2016-12-22 17:09:31 +0000

    test and package.json updated for mocha text

--- npm/test.js
+++ npm/test.js
@@ -1,1 +1,15 @@
 console.log("todo: test");
+
+var expect = require('chai').expect;
+
+var IMPLICIT = require('./index.js');
+
+describe('tests', function() {
+    it('initialisation (nothin)', function() {
+        expect(true).to.equal(true);
+        expect(!!IMPLICIT).to.equal(true);
+        console.log(IMPLICIT.getLiveGeometry_from_json);
+        expect(!!(IMPLICIT.getLiveGeometry_from_json)).to.equal(true);
+    });
+});
+
