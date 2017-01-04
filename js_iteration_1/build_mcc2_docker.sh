fa340b23770cd955fd95cc45df351d91499b628a
ynotstartups <ynotstartups@gmail.com>
2017-01-04 17:56:01 +0000

    build_mcc2_docker.sh updated for build_geometry_u (progressive update). It also uses an ENV variable for changing the path for non-deployment usage

--- js_iteration_1/build_mcc2_docker.sh
+++ js_iteration_1/build_mcc2_docker.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Deployment:
+IMPLISOLID=/src
+# Local:
+# IMPLISOLID=/home/wedesign/Documents/mp5-private/implisolid
+
 OPTIM=1
 DEV=2
 WORKER=3
@@ -51,7 +56,7 @@ done
 
 ls /lib/
 
-cd /src/js_iteration_1
+cd $IMPLISOLID/js_iteration_1
 
 BOOST_FOLDER="/lib/boost_1_61_0"
 EIGEN_LIB_FOLDER="/lib/eigen"
@@ -68,7 +73,7 @@ em++ \
     -Oz \
     -s OUTLINING_LIMIT=100000 \
     -DNDEBUG -DBOOST_UBLAS_NDEBUG -DBOOST_DISABLE_ASSERTS  \
-    -s EXPORTED_FUNCTIONS="['_produce_object_old2', '_main', '_build_geometry', '_get_v_size', '_get_f_size', '_get_f', '_get_v', '_finish_geometry', '_get_f_ptr', '_get_v_ptr',   '_set_object', '_unset_object', '_set_x', '_unset_x', '_calculate_implicit_values', '_get_values_ptr', '_get_values_size', '_calculate_implicit_gradients', '_get_gradients_ptr', '_get_gradients_size', '_get_pointset_ptr', '_get_pointset_size', '_about' ]" \
+    -s EXPORTED_FUNCTIONS="['_produce_object_old2', '_main', '_build_geometry', '_get_v_size', '_get_f_size', '_get_f', '_get_v', '_finish_geometry', '_get_f_ptr', '_get_v_ptr',   '_set_object', '_unset_object', '_set_x', '_unset_x', '_calculate_implicit_values', '_get_values_ptr', '_get_values_size', '_calculate_implicit_gradients', '_get_gradients_ptr', '_get_gradients_size', '_get_pointset_ptr', '_get_pointset_size', '_build_geometry_u', '_about' ]" \
     -s NO_EXIT_RUNTIME=1          \
     -Winline         \
     -s TOTAL_MEMORY=30100100    \
@@ -76,6 +81,7 @@ em++ \
     -s ALLOW_MEMORY_GROWTH=1 \
     -s DISABLE_EXCEPTION_CATCHING=0  \
     -s DEMANGLE_SUPPORT=1 \
+    -Wno-dollar-in-identifier-extension \
     -pedantic -std=c++14  \
 mcc2.cpp  \
     -o  ../build/mcc2.compiled.js
@@ -93,7 +99,7 @@ then
          -I $EIGEN_LIB_FOLDER \
         -s TOTAL_MEMORY=30100100 \
         -s ABORTING_MALLOC=0 \
-        -s EXPORTED_FUNCTIONS="['_produce_object_old2', '_main', '_build_geometry', '_get_v_size', '_get_f_size', '_get_f', '_get_v', '_finish_geometry', '_get_f_ptr', '_get_v_ptr',   '_set_object', '_unset_object', '_set_x', '_unset_x', '_calculate_implicit_values', '_get_values_ptr', '_get_values_size', '_calculate_implicit_gradients', '_get_gradients_ptr', '_get_gradients_size', '_get_pointset_ptr', '_get_pointset_size', '_about' ]" \
+        -s EXPORTED_FUNCTIONS="['_produce_object_old2', '_main', '_build_geometry', '_get_v_size', '_get_f_size', '_get_f', '_get_v', '_finish_geometry', '_get_f_ptr', '_get_v_ptr',   '_set_object', '_unset_object', '_set_x', '_unset_x', '_calculate_implicit_values', '_get_values_ptr', '_get_values_size', '_calculate_implicit_gradients', '_get_gradients_ptr', '_get_gradients_size', '_get_pointset_ptr', '_get_pointset_size', '_build_geometry_u', _about' ]" \
         -s NO_EXIT_RUNTIME=1 \
         -s DEMANGLE_SUPPORT=1 \
         -s ASSERTIONS=1 \
