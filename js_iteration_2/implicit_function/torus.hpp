ba25b68a3145038a9c7b10cd834bbc273d039f1d
Blobixx <shane_nana@hotmail.com>
2017-01-23 14:28:07 +0000

    Torus has now a SDF implicit function. The smooth-union works perfectly well with the other SDF primitives (cube, sphere, cylinder)

--- js_iteration_2/implicit_function/torus.hpp
+++ js_iteration_2/implicit_function/torus.hpp
@@ -47,9 +47,9 @@ public:
     }
 
     torus(REAL matrix12[12]){
-        this->r = 4.;
+        this->r = 1.;
         this->rx = 0.2;
-        this->ry = 0.2;
+        this->ry = 0.3;
         this->rz = 0.2;
 
         this->transf_matrix = new REAL [12];
@@ -91,8 +91,7 @@ public:
 
             //REAL f = (1 - std::pow( r - std::pow((std::pow(x / rx, 2) + std::pow(y / ry,2)),0.5),2) - std::pow(z / rz, 2));
             //SDF
-            REAL t = 0.1;
-            (*f_output)[output_ctr] = ry - euclidean_norm((euclidean_norm(x,z) -rx)*(euclidean_norm(x,z)-rx), y);
+            (*f_output)[output_ctr] = 0.2 - euclidean_norm((euclidean_norm(x,y) -0.5), z);
         }
     }
 
@@ -114,12 +113,19 @@ public:
             REAL x = (*i)[0];
             REAL y = (*i)[1];
             REAL z = (*i)[2];
-
+            /*
             REAL a = std::pow(x*x/(rx*rx) + y*y/(ry*ry), 0.5);
 
             REAL gx = (2 * x /(rx*rx*a)) * (r - a);
             REAL gy = (2 * y / (ry*ry*a)) * (r - a);
-            REAL gz = -2*z/(rz*rz);
+            REAL gz = -2*z/(rz*rz);*/
+
+            REAL A = euclidean_norm((euclidean_norm(x,y) -0.5), z);
+            REAL B = euclidean_norm(x,y) -0.5;
+            REAL C = euclidean_norm(x,y);
+            REAL gx = - (x*B)/(C*A);
+            REAL gy = -(y*B)/(C*A);
+            REAL gz = - z/A;
 
             (*output)[output_ctr][0] = this->inv_transf_matrix[0]*gx + this->inv_transf_matrix[4]*gy + this->inv_transf_matrix[8]*gz;
             (*output)[output_ctr][1] = this->inv_transf_matrix[1]*gx + this->inv_transf_matrix[5]*gy + this->inv_transf_matrix[9]*gz;
