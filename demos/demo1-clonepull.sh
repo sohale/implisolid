1a40f4c8b2f15b50dac10f7ac1b6c880327ed398
Sohail Siadatnejad <sohail.siadatnejad@lloydsbanking.com>
2022-04-16 00:46:03 +0100

    correct the clonepull script

--- demos/demo1-clonepull.sh
+++ demos/demo1-clonepull.sh
@@ -1,16 +1,40 @@
-
 #!/bin/bash
 
 # run:  bash deploy-demo-1.sh
+
+# This script downloads the prequisites
+#
+#
+
+# Parameters
+#export BASELOC1=/Users/$USER/cs/mp5
+#export BASELOC2=/Users/$USER/cs/mp5
+export BASELOC1=/Users/$USER/cs
+export BASELOC2=/Users/$USER/cs
+
+export IMPLISOLID=$BASELOC1/implisolid
+export MP5_PRIVATE=$BASELOC2/mp5-private
+
+# manually:
+# reset
+#
+# rm -rf $IMPLISOLID
+# rm -rf $MP5_PRIVATE
+# first time
+#cd $BASELOC1
+#git clone git@github.com:sohale/implisolid.git
+#cd $BASELOC2
+#git clone git@github.com:sohale/mp5-private.git # not used here
+
 set -e
 
 # target:
-export DEMO_LOCATION=/Users/$USER/cs/mp5/implisolid/demos/demo1
-export BUILD_LOCATION=/Users/$USER/cs/mp5/implisolid/demos/build
+export DEMO_LOCATION=$IMPLISOLID/demos/demo1
+export BUILD_LOCATION=$IMPLISOLID/demos/build
+mkdir -p $DEMO_LOCATION
+mkdir -p $BUILD_LOCATION
 
 #sources:
-export MP5_PRIVATE=/Users/$USER/cs/mp5/mp5-private
-export IMPLISOLID=/Users/$USER/cs/mp5/implisolid
 export DEMO0=$IMPLISOLID/js_iteration_2/examples/mp5interactive
 export JSI2=$IMPLISOLID/js_iteration_2
 export JS_EX1=$IMPLISOLID/js_iteration_2/examples/js
@@ -22,13 +46,15 @@ export BUILT=$IMPLISOLID/docs/implisolid-build
 # ...
 
 prime_docker() {
+  echo "docker pull emscripten/emsdk"
   docker pull emscripten/emsdk
 }
 echo "\n\n\n"
 
 cd $BUILD_LOCATION
 pwd
-mkdir -p lib
+mkdir -p $BUILD_LOCATION/lib
+export LIBDIR=$IMPLISOLID/demos/build/lib
 
 get_boost() {
     echo "downloading library: Boost (1.75.0)"
@@ -36,7 +62,10 @@ get_boost() {
     # check https://www.boost.org/users/download/
     # old: boost_1_61_0
 
-    wget https://dl.bintray.com/boostorg/release/1.75.0/source/boost_1_75_0.tar.gz -O boost.tar.gz
+    # two alternatives
+
+    #wget https://dl.bintray.com/boostorg/release/1.75.0/source/boost_1_75_0.tar.gz -O boost.tar.gz
+    wget https://boostorg.jfrog.io/artifactory/main/release/1.75.0/source/boost_1_75_0.tar.gz -O boost.tar.gz
 
     # boost_1_75_0.tar.gz
     echo Unzipping boost\'s .tar.gz
@@ -49,7 +78,12 @@ get_boost() {
 
     # todo: wget filename
     # wget --server-response -q -O - "https://very.long/url/here" 2>&1 |   grep "Content-Disposition:" | tail -1 |   awk 'match($0, /filename=(.+)/, f){ print f[1] }' )
+
 }
+#get_boost
+export BOOST=$LIBDIR/boost_1_75_0
+echo "Boost downloaded in: $BOOST"
+
 
 get_eigen() {
   echo "downloading library: Eigen  (3.?.?)"
@@ -58,12 +92,20 @@ get_eigen() {
 
   # todo: 831133cc =  3.4.0-rc1
   # master latest was: 853a5c4b843a3f1de5de2a25429eefd62dbd153a
-  git clone https://gitlab.com/libeigen/eigen.git  --depth 1
+
+  # two alternatives
+  #git clone https://gitlab.com/libeigen/eigen.git  --depth 1
+  git clone https://github.com/libigl/eigen  --depth 1
+
   #mv -vn ./eigen/Eigen ./lib/eigen
   mv -vn ./eigen ./lib/
 }
 
-get_eigen
+#get_eigen
+export EIGEN=$LIBDIR/eigen
+echo "Eigen downloaded in: $EIGEN"
+
+prime_docker
 
 # prepare
 
