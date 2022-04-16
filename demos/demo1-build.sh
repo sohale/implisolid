867aa1ec608ecc8d8c533e62be5e7e0c1162cf64
Sohail Siadatnejad <sohale@gmail.com>
2022-04-16 17:17:51 +0100

    debug: overhaul in progress

--- demos/demo1-build.sh
+++ demos/demo1-build.sh
@@ -3,10 +3,10 @@
 
 # args:
 # pwd: used for $REPO_ROOT
-assert_env_nonempty SCRIPTS_DIR "env-argument SCRIPTS_DIR missing. Must contain \$SCRIPTS_DIR/build_configuration.sh"
+assert_env_nonempty $SCRIPTS_DIR "env-argument SCRIPTS_DIR missing. Must contain \$SCRIPTS_DIR/build_configuration.sh"
 # build script folder, different to deploy etc
 
-assert_env_nonempty IMPLISOLID "env-argument IMPLISOLID ..."
+assert_env_nonempty $IMPLISOLID "env-argument IMPLISOLID ..."
 #export IMPLISOLID=$BASELOC1/implisolid
 
 
@@ -29,9 +29,9 @@ set -e
 IMPLISOLID=$IMPLISOLID source $SCRIPTS_DIR/build_configuration.sh
 # output: DEMO_LOCATION, BUILD_LOCATION,LIB_FOLDER
 
-assert_env_nonempty DEMO_LOCATION "env-argument DEMO_LOCATION ..."
-assert_env_nonempty BUILD_LOCATION "env-argument BUILD_LOCATION ..."
-assert_env_nonempty LIB_FOLDER "env-argument LIB_FOLDER ..."
+assert_env_nonempty $DEMO_LOCATION "env-argument DEMO_LOCATION ..."
+assert_env_nonempty $BUILD_LOCATION "env-argument BUILD_LOCATION ..."
+assert_env_nonempty $LIB_FOLDER "env-argument LIB_FOLDER ..."
 
 # Parameters, from the specific configuration (relative locaation of build, lib, etc):
 # target:
