042c5dec40e5552d6edb928876e6021e3adff08c
Sohail Siadatnejad <sohale@gmail.com>
2022-04-30 15:24:09 +0100

    bake script (empty for now)

---
+++ scripts/demos/demo1/bake-into-implisolid-build-and-push.bash
@@ -0,0 +1,7 @@
+
+# puts/deploys into the submodule folder
+# updates the submodule commit num  https://github.com/sohale/implisolid-build/tree/...
+# pushes the submomdule
+# maybe: removes innecessarily log commit depth (custom)
+# pushes to the right github pages
+# pull and republish the sohale.github.io repo
