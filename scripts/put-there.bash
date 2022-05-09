f5a89aa985d0c9fd2540958785611a39e2587134
Sohail Siadatnejad <sohale@gmail.com>
2022-05-09 14:49:52 +0100

    put-these.bash script in env (code as data)

--- scripts/put-there.bash
+++ scripts/put-there.bash
@@ -1,7 +1,10 @@
 
-U="****"
-DM="****"
+U="ssthere"
+DM="157.245.40.6"
 #ssh $U@$DM -- 'find /'
 ssh $U@$DM -- 'ls ~/.ssh/'
-#ssh $U@$DM -- 'mkdir -p ~/implisolid-on-prod; git clone hhttps://github.com/sohale/implisolid.git  ~/implisolid-on-prod/implisolid ; cd ~/implisolid-on-prod/implisolid; bash ./scripts/e2e-test-builds.bash'
-
+# make it one-liner
+SCRIPT_TO_RUN=<<< MYSCRIPT
+mkdir -p ~/implisolid-on-prod; git clone https://github.com/sohale/implisolid.git  ~/implisolid-on-prod/implisolid ; cd ~/implisolid-on-prod/implisolid; bash ./scripts/e2e-test-builds.bash
+MYSCRIPT
+#ssh $U@$DM -- "$SCRIPT_TO_RUN"
