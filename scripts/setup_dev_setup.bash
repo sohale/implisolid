6a9e3a885547b31b5ce3d7d1a691f6ffae624c2a
Sohail Siadat <sohale@gmail.com>
2026-01-17 20:56:03 +0000

    fix: emscripten.h progress: 0 errors!

--- scripts/setup_dev_setup.bash
+++ scripts/setup_dev_setup.bash
@@ -58,10 +58,15 @@ cat > "$COMPILE_DB" <<EOF
   {
     "directory": "$ORIG_REPO_ROOT",
     "file": "$SRC1",
-    "command": "$CXX -std=c++17  -I$BOOST -I$EIGEN  -isystem ${ORIG_REPO_ROOT}/${EMSDK_SURROGATE}/include -isystem ${ORIG_REPO_ROOT}/${EMSDK_SURROGATE}/include/c++/v1   --target=wasm32-unknown-emscripten -nostdinc++ js_iteration_1/mcc2.cpp"
+    "command": "$CXX -std=c++17  -I$BOOST -I$EIGEN  --sysroot=${ORIG_REPO_ROOT}/${EMSDK_SURROGATE} -isystem ${ORIG_REPO_ROOT}/${EMSDK_SURROGATE}/include/c++/v1   --target=wasm32-unknown-emscripten -nostdinc++ js_iteration_1/mcc2.cpp"
   }
 ]
 EOF
+
+# was:
+#. "$CXX -std=c++17  -I$BOOST -I$EIGEN  -isystem ${ORIG_REPO_ROOT}/${EMSDK_SURROGATE}/include -isystem ${ORIG_REPO_ROOT}/${EMSDK_SURROGATE}/include/c++/v1   --target=wasm32-unknown-emscripten -nostdinc++ js_iteration_1/mcc2.cpp"
+# This works cleanly: 0 errors:
+# "$CXX -std=c++17  -I$BOOST -I$EIGEN  --sysroot=${ORIG_REPO_ROOT}/${EMSDK_SURROGATE} -isystem ${ORIG_REPO_ROOT}/${EMSDK_SURROGATE}/include/c++/v1   --target=wasm32-unknown-emscripten -nostdinc++ js_iteration_1/mcc2.cpp"
 
 echo "compile_commands.json written to:"
 echo "  $COMPILE_DB"
