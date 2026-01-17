fd80f86c506c94ed2b9268f2b5836a3873c46347
Sohail Siadat <sohale@gmail.com>
2026-01-17 19:07:15 +0000

    fix clangd issue of missing emscripten.h include file (in progress)

--- scripts/setup_dev_setup.bash
+++ scripts/setup_dev_setup.bash
@@ -8,7 +8,7 @@ set -x
 # and let clangd be the only authority.
 
 
-
+# Note that this does not use the e2e folder that keeps a copy (scripted build), but the actual original repo's files (as required for `clangd` LSP to work in a useful way).
 export ORIG_REPO_ROOT="$(git rev-parse --show-toplevel)"
 export E2E="$ORIG_REPO_ROOT/e2e-sandbox-temp"
 echo "Repo root        : $ORIG_REPO_ROOT"
@@ -24,8 +24,24 @@ export CXX
 BOOST="$ORIG_REPO_ROOT/build/lib/boost_1_75_0"
 EIGEN="$ORIG_REPO_ROOT/build/lib/eigen"
 
+# Emscripten:
+EMSDK_SURROGATE="build/emscripten_emsdk_surrogate_for_clangd"
+export EMSDK_VERSION="3.1.14"
+# todo: unify with another similat defnition
 
-
+mkdir -p "$EMSDK_SURROGATE"
+docker run --rm \
+  -u 1000:1000 \
+  -v "${ORIG_REPO_ROOT}/${EMSDK_SURROGATE}:/out" \
+  emscripten/emsdk:${EMSDK_VERSION} \
+  bash -c '
+    set -euo pipefail
+    cp -a /emsdk/upstream/emscripten/system/include /out/
+  '
+# verify the purpose is fulfilled:
+test -f "${EMSDK_SURROGATE}/include/emscripten.h"
+# Recent versions of the SDK (3.x) moved to a model where only the headers under the cache/sysroot include tree are intended for compilation.
+# It contains sanitized copies of standard headers (libc/libc++), platform headers, and Emscripten API headers that actually work with both native and cross-compilation modes.
 
 SRC1="$ORIG_REPO_ROOT/js_iteration_1/mcc2.cpp"
 test -f "$SRC1"
@@ -37,7 +53,7 @@ cat > "$COMPILE_DB" <<EOF
   {
     "directory": "$ORIG_REPO_ROOT",
     "file": "$SRC1",
-    "command": "$CXX -std=c++17 -I$BOOST -I$EIGEN js_iteration_1/mcc2.cpp"
+    "command": "$CXX -std=c++17 -I$BOOST -I$EIGEN -I${ORIG_REPO_ROOT}/${EMSDK_SURROGATE}/include  js_iteration_1/mcc2.cpp"
   }
 ]
 EOF
